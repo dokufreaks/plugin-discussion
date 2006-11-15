4eebd364d0e8f29472ad5b047021e2e2204a8c98
lukas.zapletal <chi@chimeric.de>
2006-11-15 18:36:06 +0100

    25 czech translation - utf8 of course
    darcs-hash:20061115173606-d7814-d5eed3d9cd2456cb687499dde2265ac94c1ac6f4.gz

---
+++ lang/cs/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+
+$lang['discussion']      = 'Diskuze';
+$lang['btn_reply']       = 'Odpověď';
+$lang['btn_hide']        = 'Skrýt';
+$lang['btn_show']        = 'Zobrazit';
+$lang['wordblock']       = 'Váš příspěvek byl blokován jako spam.';
+$lang['mail_newcomment'] = 'Nový komentář';
+
+$lang['reply']           = 'Odpověď';
+$lang['replies']         = 'Odpovědi';
+$lang['newthread']       = 'Nové vlákno';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/cs/settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['allowguests']     = 'povolit komentáře neregistrovaným';
+$lang['linkemail']       = 'linkovat jména komentujících s e-maily';
+$lang['usegravatar']     = 'použit obrázky gavatar';
+
+$lang['gravatar_size']   = 'implicitní velikost gavataru';
+$lang['gravatar_rating'] = 'minimální hodnocení gavataru';
+
+$lang['threads_showdate'] = 'zobrazit datum nejnovější odpovědi v seznamu vláken';
+$lang['threads_showuser'] = 'zobrazit jméno uživatele, který založil vlákno';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
