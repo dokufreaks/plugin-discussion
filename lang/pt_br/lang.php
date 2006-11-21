ff97693f661dd14c3d9d3fb99ea02124854c58b0
wikidesign <chi@chimeric.de>
2006-11-21 14:21:40 +0100

    28 Forgot to add new files
    darcs-hash:20061121132140-89468-4625f1b8709d784239aa5a0854c1cc34ef22f965.gz

---
+++ lang/pt_br/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Brazilian Portuguese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Marcus D'Alencar <marcus_dalencar@yahoo.com.br>
+ */
+
+$lang['discussion']      = 'Discussão';
+$lang['btn_reply']       = 'Responder';
+$lang['btn_hide']        = 'Ocultar';
+$lang['btn_show']        = 'Exibir';
+$lang['wordblock']       = 'Seu comentário foi bloqueado como spam.';
+$lang['mail_newcomment'] = 'Novo Comentário';
+
+$lang['reply']           = 'Responder';
+$lang['replies']         = 'Respostas';
+$lang['newthread']       = 'Novo tópico';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
