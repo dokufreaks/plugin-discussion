b8a83a8a15da3cc5d5e2d297ab4a7c6943d6edd3
wikidesign <chi@chimeric.de>
2006-11-30 17:22:21 +0100

    41 Slovak translation added -- thanks to Marek Pirhala
    darcs-hash:20061130162221-89468-ffba9fae53d6f12356f392ce348562f8bb994d06.gz

---
+++ lang/sk/settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Slovak language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Pirhala Marek <mpirhala@genesispo.sk>
+ */
+ 
+// for the configuration manager
+$lang['allowguests']     = 'povoliť komentáre neregistrovaným';
+$lang['linkemail']       = 'linkovať mená komentujúcich s e-mailami';
+$lang['usegravatar']     = 'použiť obrázky gavatar';
+
+$lang['gravatar_size']   = 'implicitná veľkosť gavataru';
+$lang['gravatar_rating'] = 'minimálne hodnotenie gavataru';
+
+$lang['threads_showdate'] = 'zobraziť dátum najnovšej odpovede v zozname vlákien';
+$lang['threads_showuser'] = 'zobraziť meno užívateľa, ktorý založil vlákno';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
