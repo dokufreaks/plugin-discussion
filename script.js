c43d6f217e323613180b3169c5756146e914f15d
wikidesign <chi@chimeric.de>
2006-12-20 19:51:50 +0100

    72 Made script.js JSLint conform
    darcs-hash:20061220185150-89468-06bd0ea301934eee092dedfef96d09d12b54b837.gz

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function isBlank(s)
-{
-  if ( (s == null) || (s.length == 0) )
+function isBlank(s){
+  if ((s === null) || (s.length === 0)){
     return true;
+  }
 
-  for(var i = 0; i < s.length; i++)
-  {
+  for (var i = 0; i < s.length; i++){
     var c = s.charAt(i);
-	  if ((c != ' ') && (c != '\n') && (c != '\t'))
+	  if ((c != ' ') && (c != '\n') && (c != '\t')){
 	    return false;
+    }
   }
   return true;
 }
 
-function validate(frm) {
-  if (isBlank(frm.mail.value) || frm.mail.value.indexOf("@") == -1) {
+function validate(frm){
+  if (isBlank(frm.mail.value) || frm.mail.value.indexOf("@") == -1){
     frm.mail.focus();
     return false;
   }
-  if (isBlank(frm.name.value)) {
+  if (isBlank(frm.name.value)){
     frm.name.focus();
     return false;
   }
 
-  if (isBlank(frm.text.value)) {
+  if (isBlank(frm.text.value)){
     frm.text.focus();
     return false;
 	}
