106e02ebb475e6cb4c4d12d0187489eb6eb2c5f8
wikidesign <chi@chimeric.de>
2007-01-13 21:08:27 +0100

    87 Portuguese translation by Adriano Afonso
    darcs-hash:20070113200827-89468-59c773e16f38fd4345e8c49ef1e076a0c45be420.gz

---
+++ lang/pt/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Adriano Afonso <mail@adrianoafonso.net>
+*/
+ 
+// for the configuration manager
+$lang['allowguests']     = 'permitir comentários de utilizadores não registados';
+$lang['linkemail']       = 'adicionar ligação para correio electrónico dos utilizadores';
+$lang['usegravatar']     = 'utilizar gravatares dos utilizadores nos comentários';
+
+$lang['gravatar_size']   = 'tamanho padrão do gravatar';
+$lang['gravatar_rating'] = 'rating mínimo para os gravatars';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
