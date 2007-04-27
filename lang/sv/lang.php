9c2eac6c2b1c5ec9a3584e973633644058631f8c
wikidesign <chi@chimeric.de>
2007-04-27 12:26:45 +0200

    103 Corrected folder name for Swedish translation se -> sv
    darcs-hash:20070427102645-89468-20d276877d6459a21170301ec54144addb0cbc59.gz

---
+++ lang/sv/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Swedish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Christer Nygren <wiki@fronet.fro.se>
+ */
+
+$lang['discussion']      = 'Diskussion';
+$lang['comment']         = 'Kommentar';
+$lang['comments']        = 'Kommentarer';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
