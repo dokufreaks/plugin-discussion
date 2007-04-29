1772ca15e839b7d14afd0a7907758baad60fb69d
lukas.zapletal <chi@chimeric.de>
2007-04-29 23:30:28 +0200

    108 Czech translation
    darcs-hash:20070429213028-d7814-558bcf43eb0d8c7beaa866ae8c7cf4b242c6f46a.gz

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Czech language file
+ * Czech language file (UTF-8 encoding)
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
  */
  
 // for the configuration manager
 $lang['allowguests'] = 'povolit komentáře neregistrovaným';
 $lang['linkemail']   = 'linkovat jména komentujících s e-maily';
-$lang['useavatar']   = 'použit obrázky avatar';
+$lang['useavatar']   = 'použit avatar obrázky';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
