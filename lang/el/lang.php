38aeb6f2f351cc75d9e1d460207523680f6d6189
Gina Haeussge <osd@foosel.net>
2008-04-03 20:41:26 +0200

    Added greek translation
    darcs-hash:20080403184126-2b4f5-c4e29939cac18f1ed4d58e2ed72c7be99c66263d.gz

---
+++ lang/el/lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Greek language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     chomwitt <aprekates@gmail.com>
+ */
+ 
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+ 
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu']            = 'Συντονισμός Συζητήσης'; 
+$lang['discussion']      = 'Συζήτηση';
+$lang['comment']         = 'Σχόλιο';
+$lang['comments']        = 'Σχόλια';
+$lang['url']             = 'Ιστότοπος';
+$lang['address']         = 'Διεύθυνση';
+$lang['date']            = 'Ημερομηνία';
+$lang['entercomment']    = 'Εισήγαγε το σχολιό σου';
+$lang['wikisyntax']      = 'επιτρέπετε βικι-συντακτικό';
+$lang['older']           = 'Παλιότερες συζητήσεις';
+$lang['newer']           = 'Νεότερες συζητήσεις';
+$lang['btn_reply']       = 'Απάντηση';
+$lang['btn_hide']        = 'Κρύψε';
+$lang['btn_show']        = 'Εμφάνισε';
+$lang['btn_change']      = 'Αλλαξε';
+$lang['statuschanged']   = 'Άλλαξε η κατάσταση της συζήτησης';
+$lang['wordblock']       = 'Το σχόλιο σου μπλοκαρίστικε σαν άχρηστο.';
+$lang['mail_newcomment'] = 'Νεο σχόλιο';
+
+$lang['status']          = 'Κατάσταση';
+$lang['off']             = 'ανενεργό';
+$lang['open']            = 'ανοιχτό';
+$lang['closed']          = 'κλειστό';
+
+$lang['reply']           = 'Απάντηση';
+$lang['replies']         = 'Απαντήσεις';
+$lang['newthread']       = 'Νεο νήμα';
+$lang['threads']         = 'Νήματα';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/el/settings.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Hellas language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     chomwitt <aprekates@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['automatic']    = 'Τμήμα συζήτησης σε κάθε σελίδα σαν προεπιλογή ';
+$lang['allowguests']  = 'επέτρεψε σχόλια σε μη καταχωρημένους χρήστες';
+$lang['linkemail']    = 'σύνδεσε όνομα σχολιαστή με  e-mail';
+$lang['useavatar']    = 'χρησιμοποίησε εικόνες αβατάρ χρήστη για σχόλεια';
+$lang['urlfield']     = 'επέτρεψε εισαγωγή  URL από χρήστες';
+$lang['addressfield'] = 'επέτρεψε τους χρήστες να εισάγουν διεύθυνση ταχυδρομίου';
+$lang['adminimport']  = 'επέτρεψε τους διαχειριστές να θέσουν προς εισαγωγή όλα τα πεδία';
+$lang['usecocomment'] = 'χρησιμοποίησε ανίχνευση σχολίων coComment';
+$lang['wikisyntaxok'] = 'επέτρεψε βίκι συντακτικό στα σχόλια';
+
+$lang['threads_formposition']          = 'θέση φόρμας νέου νήματος';
+$lang['threads_formposition_o_top']    = 'πάνω';
+$lang['threads_formposition_o_bottom'] = 'κάτω';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
