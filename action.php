c3a09fa6be8a5bdab487556ea1bf62ce743e15c1
Gina Haeussge <osd@foosel.net>
2008-04-19 18:26:16 +0200

    fix for discussions not showing when breadcrumbs contain page containing a blog (2nd edition)
    darcs-hash:20080419162616-2b4f5-ade460430adc4d832208d5f22941c7f9ed5d7faf.gz

--- action.php
+++ action.php
@@ -106,8 +106,6 @@ class action_plugin_discussion extends DokuWiki_Action_Plugin{
    * Shows all comments of the current page
    */
   function _show($reply = NULL, $edit = NULL){
-    if ($this->_ignore()) return false;
-
     global $ID, $INFO, $ACT;
     
     if ($ACT !== 'show') return false;
@@ -842,8 +840,6 @@ class action_plugin_discussion extends DokuWiki_Action_Plugin{
    * Finds out whether there is a discussion section for the current page
    */
   function _hasDiscussion(&$title){
-    //if ($this->_ignore()) return false;
-    
     global $ID;
         
     $cfile = metaFN($ID, '.comments');
@@ -859,21 +855,6 @@ class action_plugin_discussion extends DokuWiki_Action_Plugin{
     $num = $comments['number'];
     if ((!$comments['status']) || (($comments['status'] == 2) && (!$num))) return false;
     else return true;
-  }
-  
-  /**
-   * Checks whether a discussion section request should be ignored
-   *
-   * Include Plugin or Pagelist Plugin might load .comments without actually
-   * wanting to add a discussion section for the *including* page
-   */
-  function _ignore(){
-    $ignoreClasses   = array('helper_plugin_include', 'helper_plugin_pagelist');
-    $declaredClasses = get_declared_classes();
-    foreach ($ignoreClasses as $ignoreClass){
-      if (in_array($ignoreClass, $declaredClasses)) return true;
-    }
-    return false;
   }
   
   /**
