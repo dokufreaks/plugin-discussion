0cd03c19d769518e754d5a30719f9ae6874f857e
Michael Klier <chi@chimeric.de>
2009-02-10 00:00:29 +0100

    nicer comment preview
    darcs-hash:20090209230029-23886-35840891fab5a7ae35229d363617e7cf149b059a.gz

--- ajax.php
+++ ajax.php
@@ -12,11 +12,12 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_INC.'inc/init.php');
 require_once(DOKU_INC.'inc/parserutils.php');
 
-// FIXME session token?
 print p_locale_xhtml('preview');
+print '<div class="comment_preview">';
 if($_REQUEST['wikisyntaxok']) {
     print p_render('xhtml', p_get_instructions($_REQUEST['comment']), $info);
 } else {
     print hsc($_REQUEST['comment']);
 }
+print '</div>';
 // vim:ts=4:sw=4:et:enc=utf-8:
