068d71ced5ceb81d53af66d0a8aa9fc98390e11c
Michael Klier <chi@chimeric.de>
2009-08-24 14:19:25 +0200

    updated russion translation

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,22 +4,46 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Mike Lykov <lykov.myu@mgsm.ru>
+ * @author     Maxim Maximov <mxmv@@mail.ru>
  */
 
-$lang['discussion']      = 'Р”РёСЃРєСѓСЃСЃРёСЏ';
-$lang['comment']         = 'РљРѕРјРјРµРЅС‚Р°СЂРёР№';
-$lang['comments']        = 'РљРѕРјРјРµРЅС‚Р°СЂРёРµРІ';
-$lang['url']             = 'Р’РµР±СЃР°Р№С‚';
-$lang['address']         = 'РђРґСЂРµСЃ';
-$lang['date']            = 'Р”Р°С‚Р°';
-$lang['btn_reply']       = 'РћС‚РІРµС‚РёС‚СЊ';
-$lang['btn_hide']        = 'РЎРєСЂС‹С‚СЊ';
-$lang['btn_show']        = 'РџРѕРєР°Р·Р°С‚СЊ';
-$lang['wordblock']       = 'Р’Р°С€ РєРѕРјРјРµРЅС‚ Р·Р°Р±Р»РѕРєРёСЂРѕРІР°РЅ РєР°Рє СЃРїР°Рј.';
-$lang['mail_newcomment'] = 'РќРѕРІС‹Р№ РєРѕРјРјРµРЅС‚';
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
 
-$lang['reply']           = 'РћС‚РІРµС‚';
-$lang['replies']         = 'РћС‚РІРµС‚С‹';
-$lang['newthread']       = 'РќРѕРІР°СЏ РІРµС‚РєР°';
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu']            = 'Модерирование обсуждений';
+
+$lang['discussion']      = 'Обсуждение';
+$lang['comment']         = 'Комментарий';
+$lang['comments']        = 'Комментариев';
+$lang['nocomments']      = 'Комментариев';
+$lang['url']             = 'Вебсайт';
+$lang['address']         = 'Адрес';
+$lang['date']            = 'Дата';
+$lang['entercomment']    = 'Введите ваш комментарий';
+$lang['wikisyntax']      = 'wiki-синтаксис разрешен';
+$lang['older']           = 'Более старые обсуждения';
+$lang['newer']           = 'Более новые обсуждения';
+$lang['btn_reply']       = 'Ответить';
+$lang['btn_hide']        = 'Скрыть';
+$lang['btn_show']        = 'Показать';
+$lang['btn_change']      = 'Изменить';
+$lang['statuschanged']   = 'Статус обсуждения изменен';
+$lang['wordblock']       = 'Ваш комментарий заблокирован как спам.';
+$lang['moderation']      = 'Ваш комментарий был добавлен в очередь на модерацию.';
+$lang['mail_newcomment'] = 'Новый коммент';
+$lang['subscribe']       = 'Подписаться на обсуждение';
+
+$lang['status']          = 'Статус';
+$lang['off']             = 'отключено';
+$lang['open']            = 'открыто';
+$lang['closed']          = 'закрыто';
+
+$lang['reply']           = 'Ответ';
+$lang['replies']         = 'Ответы';
+$lang['newthread']       = 'Новая ветка';
+$lang['threads']         = 'Ветки';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
