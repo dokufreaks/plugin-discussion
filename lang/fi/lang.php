b5824c2a11e93ececdcb0a414eaed4ba1e32d940
Tuomas Korkalainen <kriz@iwn.cx>
2010-03-09 13:53:41 +0100

    added finish translation

---
+++ lang/fi/lang.php
@@ -0,0 +1,48 @@
+﻿<?php
+/**
+ * Finnish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Tuomas Korkalainen <kriz@iwn.cx>
+ */
+ 
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+ 
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu']            = 'Tarkasta keskustelut'; 
+
+$lang['discussion']      = 'Keskustelu';
+$lang['comment']         = 'Kommentti';
+$lang['comments']        = 'Kommenttia';
+$lang['nocomments']      = 'Kommenttia';
+$lang['url']             = 'WWW-sivu';
+$lang['address']         = 'Osoite';
+$lang['date']            = 'päivämäärä';
+$lang['entercomment']    = 'Kirjoita kommentti';
+$lang['wikisyntax']      = 'wiki syntaksi sallitaan';
+$lang['older']           = 'Vanhemmat keskustelut';
+$lang['newer']           = 'Uudemmat keskustelut';
+$lang['btn_reply']       = 'Vastaa';
+$lang['btn_hide']        = 'Piilota';
+$lang['btn_show']        = 'Näytä';
+$lang['btn_change']      = 'Muuta';
+$lang['statuschanged']   = 'Keskustelun tila muuttunut';
+$lang['wordblock']       = 'Kommenttisi estettiin, koska se vaikutti roskapostilta.';
+$lang['moderation']      = 'Kommenttisi on lisätty ja se odottaa tarkastamista.';
+$lang['mail_newcomment'] = 'Uusi kommentti';
+$lang['subscribe']       = 'Tilaa viestiketjuun tulevat kommentit sähköpostiin';
+
+$lang['status']          = 'Tila';
+$lang['off']             = 'ei käytössä';
+$lang['open']            = 'avoin';
+$lang['closed']          = 'suljettu';
+
+$lang['reply']           = 'Vastaa';
+$lang['replies']         = 'Vastausta';
+$lang['newthread']       = 'Uusi viestiketju';
+$lang['threads']         = 'Ketjua';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
