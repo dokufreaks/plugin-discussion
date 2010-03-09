0442ed9491af483152de2cbf43ea557c97a6c201
Sergio Evandro Motta <sergio@cisne.com.br>
2010-03-09 13:57:15 +0100

    updated pt-br translation

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -4,19 +4,46 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Marcus D'Alencar <marcus_dalencar@yahoo.com.br>
+ * @author     Sergio Evandro Motta <sergio@cisne.com.br>
  */
+
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu']            = 'Moderar Discussões';
 
 $lang['discussion']      = 'Discussão';
 $lang['comment']         = 'Comentário';
 $lang['comments']        = 'Comentários';
+$lang['nocomments']      = 'Comentários';
+$lang['url']             = 'Website';
+$lang['address']         = 'Endereço';
+$lang['date']            = 'Data';
+$lang['entercomment']    = 'Entre com o seu comentário';
+$lang['wikisyntax']      = 'Sintaxe wiki é permitida';
+$lang['older']           = 'Antigas discussões';
+$lang['newer']           = 'Novas discussões';
 $lang['btn_reply']       = 'Responder';
 $lang['btn_hide']        = 'Ocultar';
 $lang['btn_show']        = 'Exibir';
+$lang['btn_change']      = 'Alterar';
+$lang['statuschanged']   = 'Status da discussão alterado';
 $lang['wordblock']       = 'Seu comentário foi bloqueado como spam.';
+$lang['moderation']      = 'Seu comentário foi adicionado e aguarda moderação.';
 $lang['mail_newcomment'] = 'Novo Comentário';
+$lang['subscribe']       = 'Inscrever-se nos comentários';
+
+$lang['status']          = 'Status';
+$lang['off']             = 'encerrada';
+$lang['open']            = 'aberta';
+$lang['closed']          = 'fechada';
 
 $lang['reply']           = 'Resposta';
 $lang['replies']         = 'Respostas';
 $lang['newthread']       = 'Novo tópico';
+$lang['threads']         = 'Tópicos';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
