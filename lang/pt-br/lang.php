89d0cb9872b8220caffd243f614670f29adb4d1c
Sidnei <sidnei@sidnei.org>
2010-07-15 11:52:45 +0200

    updated pt-br translation

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,19 +1,18 @@
 <?php
 /**
- * Brazilian Portuguese language file
+ * Brazilian portuguese language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Marcus D'Alencar <marcus_dalencar@yahoo.com.br>
- * @author     Sergio Evandro Motta <sergio@cisne.com.br>
+ * @author     Sidnei Neves <sidnei@sidnei.org>
  */
-
+ 
 // settings must be present and set appropriately for the language
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
-
+ 
 // for admin plugins, the menu prompt to be displayed in the admin menu
 // if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Moderar Discussões';
+$lang['menu']            = 'Moderate Discussions'; 
 
 $lang['discussion']      = 'Discussão';
 $lang['comment']         = 'Comentário';
@@ -22,28 +21,28 @@ $lang['nocomments']      = 'Comentários';
 $lang['url']             = 'Website';
 $lang['address']         = 'Endereço';
 $lang['date']            = 'Data';
-$lang['entercomment']    = 'Entre com o seu comentário';
-$lang['wikisyntax']      = 'Sintaxe wiki é permitida';
-$lang['older']           = 'Antigas discussões';
-$lang['newer']           = 'Novas discussões';
+$lang['entercomment']    = 'Insira seu comentário';
+$lang['wikisyntax']      = 'Sintaxe de wiki é permitida';
+$lang['older']           = 'Discussões antigas';
+$lang['newer']           = 'Discussões recentes';
 $lang['btn_reply']       = 'Responder';
 $lang['btn_hide']        = 'Ocultar';
-$lang['btn_show']        = 'Exibir';
-$lang['btn_change']      = 'Alterar';
+$lang['btn_show']        = 'Mostrar';
+$lang['btn_change']      = 'Trocar';
 $lang['statuschanged']   = 'Status da discussão alterado';
 $lang['wordblock']       = 'Seu comentário foi bloqueado como spam.';
-$lang['moderation']      = 'Seu comentário foi adicionado e aguarda moderação.';
-$lang['mail_newcomment'] = 'Novo Comentário';
-$lang['subscribe']       = 'Inscrever-se nos comentários';
+$lang['moderation']      = 'Seu comentário foi adicionado e aguarda a moderação.';
+$lang['mail_newcomment'] = 'Novo comentário';
+$lang['subscribe']       = 'Acompanhar comentários';
 
 $lang['status']          = 'Status';
-$lang['off']             = 'encerrada';
-$lang['open']            = 'aberta';
-$lang['closed']          = 'fechada';
+$lang['off']             = 'desligado';
+$lang['open']            = 'aberto';
+$lang['closed']          = 'fechado';
 
-$lang['reply']           = 'Resposta';
+$lang['reply']           = 'Responder';
 $lang['replies']         = 'Respostas';
-$lang['newthread']       = 'Novo tópico';
-$lang['threads']         = 'Tópicos';
+$lang['newthread']       = 'Nova discussão';
+$lang['threads']         = 'Discussões';
 
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+//Setup Notepad++ enc=utf-8 :
