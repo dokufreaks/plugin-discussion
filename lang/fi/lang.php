0158ad9984ce764899f201846b4a0710017f1c96
Erkki Joensuu <erkki.joensuu@gmail.com>
2010-09-25 12:10:02 +0200

    updated finish translation

--- lang/fi/lang.php
+++ lang/fi/lang.php
@@ -3,7 +3,7 @@
  * Finnish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Tuomas Korkalainen <kriz@iwn.cx>
+ * @author     Titorelli
  */
  
 // settings must be present and set appropriately for the language
@@ -12,37 +12,40 @@ $lang['direction']  = 'ltr';
  
 // for admin plugins, the menu prompt to be displayed in the admin menu
 // if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tarkasta keskustelut'; 
-
+ 
+// Titorelli's code
+$lang['menu']            = 'Menu'; 
+ 
 $lang['discussion']      = 'Keskustelu';
 $lang['comment']         = 'Kommentti';
-$lang['comments']        = 'Kommenttia';
-$lang['nocomments']      = 'Kommenttia';
-$lang['url']             = 'WWW-sivu';
+$lang['comments']        = 'Kommentit';
+$lang['nocomments']      = 'ei kommentteja';
+ 
+$lang['url']             = 'Web-sivu';
 $lang['address']         = 'Osoite';
-$lang['date']            = 'päivämäärä';
-$lang['entercomment']    = 'Kirjoita kommentti';
-$lang['wikisyntax']      = 'wiki syntaksi sallitaan';
-$lang['older']           = 'Vanhemmat keskustelut';
-$lang['newer']           = 'Uudemmat keskustelut';
+$lang['date']            = 'Päiväys';
 $lang['btn_reply']       = 'Vastaa';
-$lang['btn_hide']        = 'Piilota';
+$lang['btn_hide']        = 'Kätke';
 $lang['btn_show']        = 'Näytä';
-$lang['btn_change']      = 'Muuta';
-$lang['statuschanged']   = 'Keskustelun tila muuttunut';
-$lang['wordblock']       = 'Kommenttisi estettiin, koska se vaikutti roskapostilta.';
-$lang['moderation']      = 'Kommenttisi on lisätty ja se odottaa tarkastamista.';
+$lang['wordblock']       = 'Kommentit estetään roskapostina';
 $lang['mail_newcomment'] = 'Uusi kommentti';
-$lang['subscribe']       = 'Tilaa viestiketjuun tulevat kommentit sähköpostiin';
-
-$lang['status']          = 'Tila';
-$lang['off']             = 'ei käytössä';
-$lang['open']            = 'avoin';
-$lang['closed']          = 'suljettu';
-
+ 
 $lang['reply']           = 'Vastaa';
-$lang['replies']         = 'Vastausta';
-$lang['newthread']       = 'Uusi viestiketju';
-$lang['threads']         = 'Ketjua';
+$lang['replies']         = 'Vastauksia';
+$lang['newthread']       = 'Uusi säie';
+$lang['threads']         = 'Säikeitä';
+ 
+$lang['entercomment']    = 'Kirjoita kommenttisi';
+$lang['wikisyntax']      = 'wikisyntaksi sallittu';
+$lang['older']           = 'Aiemmat keskustelut';
+$lang['newer']           = 'Uudemmat keskustelut';
+$lang['btn_change']      = 'Muuta';
+$lang['statuschanged']   = 'Keskustelustatus muutettu';
+$lang['subscribe']       = 'Tilaa kommentit';
+ 
+$lang['status']          = 'Tila';
+$lang['off']             = 'Pois';
+$lang['open']            = 'Avoin';
+$lang['closed']          = 'Suljettu';
 
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+// vim:ts=4:sw=4:et:enc=utf-8: 
