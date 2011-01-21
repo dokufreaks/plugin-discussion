5addcdb2923930fc147cb3d56d76ad8a514d0f0c
lupo49 <post@lupo49.de>
2011-01-21 23:03:18 +0100

    ru: update for russian language

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -4,16 +4,30 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Mike Lykov <lykov.myu@mgsm.ru>
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
  */
  
 // for the configuration manager
-$lang['allowguests']  = 'Позволять анонимным пользователям оставлять комменты';
-$lang['linkemail']    = 'Связывать имя комментирующего с емейлом';
-$lang['useavatar']    = 'Использовать avatar-картинки для комментов';
-$lang['urlfield']     = 'Позволять пользователям вводить URL';
-$lang['addressfield'] = 'Позволять пользователям вводить почтовый адрес';
-$lang['adminimport']  = 'Позволять админам устанавливать дату коммента (для импорта)';
-$lang['usecocomment'] = 'Использовать coComment-отслеживание комментов';
-$lang['wikisyntaxok'] = 'Позволять синтаксис вики в комментах';
+$lang['automatic']    = 'Добавлять обсуждение на каждую страницу по умолчанию';
+$lang['allowguests']  = 'Разрешить анонимным пользователям оставлять комментарии';
+$lang['showguests']   = 'Разрешить незарегистрированным пользователям читать комментарии';
+$lang['linkemail']    = 'Связывать имя комментирующего с адресом его электропочты';
+$lang['useavatar']    = 'Использовать аватары для комментирующих';
+$lang['urlfield']     = 'Разрешить пользователям вводить веб-адреса';
+$lang['addressfield'] = 'Разрешить пользователям вводить адреса электропочты';
+$lang['adminimport']  = 'Разрешить администраторам устанавливать дату комментария (для импорта)';
+$lang['usecocomment'] = 'Использовать отслеживание комментариев на комментарии';
+$lang['wikisyntaxok'] = 'Разрешить вики-синтаксис в комментариях';
+$lang['subscribe']    = 'Влючить/отключить подписку на комментарии';
+$lang['newestfirst']  = 'Новые комментарии отображать первыми';
+$lang['moderate']     = 'Модерировать новые комментарии';
+
+$lang['usethreading'] = 'Разрешить ветвление комментариев';
+$lang['userealname']  = 'Отображать имя вместо логина';
+
+$lang['threads_formposition']          = 'Позиция для формы добавления комментария';
+$lang['threads_formposition_o_off']    = 'нет';
+$lang['threads_formposition_o_top']    = 'вверху';
+$lang['threads_formposition_o_bottom'] = 'внизу';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
