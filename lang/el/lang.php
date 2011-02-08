3e770db80edb7b0e8708253823ad9cc2041e4dc3
Petsagourakis George <petsagouris@gmail.com>
2011-02-08 15:13:17 +0200

    Updated and completed the Greek translation

--- lang/el/lang.php
+++ lang/el/lang.php
@@ -4,8 +4,9 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     chomwitt <aprekates@gmail.com>
+ * @author     George Petsagourakis <petsagouris@gmail.com>
  */
- 
+
 // settings must be present and set appropriately for the language
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
@@ -13,23 +14,28 @@ $lang['direction']  = 'ltr';
 // for admin plugins, the menu prompt to be displayed in the admin menu
 // if set here, the plugin doesn't need to override the getMenuText() method
 $lang['menu']            = 'Διαχείριση Συζητήσης'; 
+
 $lang['discussion']      = 'Συζήτηση';
 $lang['comment']         = 'Σχόλιο';
 $lang['comments']        = 'Σχόλια';
+$lang['nocomments']      = 'Σχόλια';
 $lang['url']             = 'Ιστότοπος';
 $lang['address']         = 'Διεύθυνση';
 $lang['date']            = 'Ημερομηνία';
-$lang['entercomment']    = 'Εισήγαγε το σχολιό σου';
-$lang['wikisyntax']      = 'επιτρέπετε βικι-συντακτικό';
-$lang['older']           = 'Παλιότερες συζητήσεις';
+$lang['entercomment']    = 'Προσθέστε το σχολιό σας';
+$lang['wikisyntax']      = 'Να επιτρέπεται ο κώδικας wiki';
+$lang['older']           = 'Παλαιότερες συζητήσεις';
 $lang['newer']           = 'Νεότερες συζητήσεις';
 $lang['btn_reply']       = 'Απάντηση';
-$lang['btn_hide']        = 'Κρύψε';
+$lang['btn_hide']        = 'Απόκρυψε';
 $lang['btn_show']        = 'Εμφάνισε';
 $lang['btn_change']      = 'Αλλαξε';
 $lang['statuschanged']   = 'Άλλαξε η κατάσταση της συζήτησης';
-$lang['wordblock']       = 'Το σχόλιο σου μπλοκαρίστικε σαν άχρηστο.';
+$lang['wordblock']       = 'Το σχόλιο σας χαρακτηρίστικε ως spam.';
+$lang['moderation']      = 'Το σχόλιο σας αναμένει έγκριση.';
 $lang['mail_newcomment'] = 'Νεο σχόλιο';
+$lang['subscribe']       = 'Να ενημερώνεστε όταν υπάρχουν νεα σχόλια σε αυτήν την σελίδα';
+$lang['noguests']        = 'Θα μπορούσατε να σχολιάσετε αν είσασταν συνδεδεμένος.';
 
 $lang['status']          = 'Κατάσταση';
 $lang['off']             = 'ανενεργό';
@@ -40,5 +46,3 @@ $lang['reply']           = 'Απάντηση';
 $lang['replies']         = 'Απαντήσεις';
 $lang['newthread']       = 'Νεο νήμα';
 $lang['threads']         = 'Νήματα';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
