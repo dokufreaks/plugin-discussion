5a71249a9ce901cdd80ae1b09f55ee690633c19d
Kwangseob Kim <mrkwangsub@gmail.com>
2011-06-10 16:53:51 +0900

    Add a new language pack for Korean

---
+++ lang/ko/settings.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     mrkwangseob<mrkwangseob@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['automatic']    = '모든 페이지에 토론란을 생성';
+$lang['allowguests']  = '비로그인 사용자에게 댓글 쓰기 허용';
+$lang['showguests']   = '비로그인 사용자에게 댓글 읽기 허용';
+$lang['linkemail']    = '댓글에 이름과 이메일을 표시';
+$lang['useavatar']    = '댓글에 사용자 이미지 표시';
+$lang['urlfield']     = '댓글 추가시 URL 필드 추가';
+$lang['addressfield'] = '댓글 추가시 주소 필드 추가';
+$lang['adminimport']  = '사용자가 댓글의 모든 항목을 입력(날짜 포함)';
+$lang['usecocomment'] = '다중 댓글 트래킹 허용';
+$lang['wikisyntaxok'] = '댓글 내용에 위키 문법 허용';
+$lang['subscribe']    = '새 댓글 알림 사용';
+$lang['newestfirst']  = '새 덧글을 먼저 오도록 배열';
+$lang['moderate']     = '댓글 개시 전에 관리자 승인 필요';
+
+$lang['usethreading'] = '댓글에 댓글 허용';
+$lang['userealname']  = '댓글에 위키에 등록한 이름 대신 댓글 등록시 사용한 이름 표기';
+
+$lang['threads_formposition']          = '새 댓글의 위치';
+$lang['threads_formposition_o_off']    = 'off';
+$lang['threads_formposition_o_top']    = 'top';
+$lang['threads_formposition_o_bottom'] = 'bottom';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
