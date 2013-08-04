8bb0b33cac0e3921a344f9cfc0f287736f9aba0d
mprins <mprins@users.sf.net>
2013-08-04 10:25:07 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,48 +1,40 @@
 <?php
+
 /**
- * Dutch language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Erwin Moller <erwin@darwine.nl>
+ * @author mprins <mprins@users.sf.net>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Modereer Discussies';
-
-$lang['discussion']      = 'Discussie';
-$lang['comment']         = 'Opmerking';
-$lang['comments']        = 'Opmerkingen';
-$lang['nocomments']      = 'Opmerkingen';
-$lang['url']             = 'Website';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Datum';
-$lang['entercomment']    = 'Geef je reactie';
-$lang['wikisyntax']      = 'Wiki-syntax is toegestaan';
-$lang['older']           = 'Oudere discussies';
-$lang['newer']           = 'Nieuwere discussies';
-$lang['btn_reply']       = 'Reageer';
-$lang['btn_hide']        = 'Verberg';
-$lang['btn_show']        = 'Laat zien';
-$lang['btn_change']      = 'Aanpassen';
-$lang['statuschanged']   = 'Discussiestatus gewijzigd';
-$lang['wordblock']       = 'Uw commentaar lijkt op spam en is geblokkerd';
-$lang['moderation']      = 'Je opmerking is toegevoegd en wacht op goedkeuring.';
-$lang['mail_newcomment'] = 'Nieuw commentaar';
-$lang['subscribe']       = 'Aanmelden voor opmerkingen via e-mail';
-$lang['noguests']        = 'Je kunt een opmerking plaatsen als je ingelogd bent.';
-
-$lang['status']          = 'Status';
-$lang['off']             = 'uit';
-$lang['open']            = 'open';
-$lang['closed']          = 'gesloten';
-
-$lang['reply']           = 'Reactie';
-$lang['replies']         = 'Reacties';
-$lang['newthread']       = 'Nieuwe discussiepagina';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'Modereer Discussies';
+$lang['discussion']            = 'Discussie';
+$lang['comment']               = 'Opmerking';
+$lang['comments']              = 'Opmerkingen';
+$lang['nocomments']            = 'Opmerkingen';
+$lang['url']                   = 'Website';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'Datum';
+$lang['entercomment']          = 'Geef je reactie';
+$lang['wikisyntax']            = 'Wiki-syntax is toegestaan';
+$lang['older']                 = 'Oudere discussies';
+$lang['newer']                 = 'Nieuwere discussies';
+$lang['btn_reply']             = 'Reageer';
+$lang['btn_hide']              = 'Verberg';
+$lang['btn_show']              = 'Laat zien';
+$lang['btn_change']            = 'Aanpassen';
+$lang['statuschanged']         = 'Discussiestatus gewijzigd';
+$lang['wordblock']             = 'Uw commentaar lijkt op spam en is geblokkerd';
+$lang['moderation']            = 'Je opmerking is toegevoegd en wacht op goedkeuring.';
+$lang['mail_newcomment']       = 'Nieuw commentaar';
+$lang['subscribe']             = 'Aanmelden voor opmerkingen via e-mail';
+$lang['noguests']              = 'Je kunt een opmerking plaatsen als je ingelogd bent.';
+$lang['status']                = 'Status';
+$lang['off']                   = 'uit';
+$lang['open']                  = 'open';
+$lang['closed']                = 'gesloten';
+$lang['reply']                 = 'Reactie';
+$lang['replies']               = 'Reacties';
+$lang['newthread']             = 'Nieuwe discussiepagina';
+$lang['toggle_display']        = 'Verberg / Laat zien';
