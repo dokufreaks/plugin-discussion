3f1eda20295dee5a88a15a436590cb3bcdf4ca21
Hideaki SAWADA <chuno@live.jp>
2013-09-18 17:40:07 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -1,50 +1,41 @@
 <?php
+
 /**
- * Japanese language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Ikuo Obataya <I.Obataya@gmail.com>
  * @author     Yusuke Ishida <yusuke.ishida.gnl@gmail.com>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'すべてのコメントを見る...'; 
-
-$lang['discussion']      = 'コメント';
-$lang['comment']         = 'コメント';
-$lang['comments']        = 'コメント';
-$lang['nocomments']      = 'コメント';
-$lang['url']             = 'ウェブサイト';
-$lang['address']         = '住所';
-$lang['date']            = '日付';
-$lang['entercomment']    = 'コメントを入力';
-$lang['wikisyntax']      = 'Wiki文法が有効です';
-$lang['older']           = '古いコメント';
-$lang['newer']           = '新しいコメント';
-$lang['btn_reply']       = '返事';
-$lang['btn_hide']        = '隠す';
-$lang['btn_show']        = '表示';
-$lang['btn_change']      = '変更';
-$lang['statuschanged']   = '状態が変更されました';
-$lang['wordblock']       = 'コメントはスパムとしてブロックされました。';
-$lang['moderation']      = 'コメントを受け付けました。管理者の確認待ちです。';
-$lang['mail_newcomment'] = '新しいコメント';
-$lang['subscribe']       = 'コメントの購読';
-$lang['noguests']        = 'ログインするとコメントできます。';
-
-$lang['status']          = '状態';
-$lang['off']             = '非表示';
-$lang['open']            = 'オープン';
-$lang['closed']          = 'クローズ';
-
-$lang['reply']           = '返事';
-$lang['replies']         = '返事';
-$lang['newthread']       = '新しいスレッド';
-$lang['threads']         = 'スレッド';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'コメントを承認する';
+$lang['discussion']            = 'コメント';
+$lang['comment']               = 'コメント';
+$lang['comments']              = 'コメント';
+$lang['nocomments']            = 'コメント';
+$lang['url']                   = 'ウェブサイト';
+$lang['address']               = '住所';
+$lang['date']                  = '日付';
+$lang['entercomment']          = 'コメントを入力';
+$lang['wikisyntax']            = 'Wiki文法が有効です';
+$lang['older']                 = '古いコメント';
+$lang['newer']                 = '新しいコメント';
+$lang['btn_reply']             = '返事';
+$lang['btn_hide']              = '隠す';
+$lang['btn_show']              = '表示';
+$lang['btn_change']            = '変更';
+$lang['statuschanged']         = '状態が変更されました';
+$lang['wordblock']             = 'コメントはスパムとしてブロックされました。';
+$lang['moderation']            = 'コメントを受け付けました。管理者の確認待ちです。';
+$lang['mail_newcomment']       = '新しいコメント';
+$lang['subscribe']             = 'コメントの購読';
+$lang['noguests']              = 'ログインするとコメントできます。';
+$lang['status']                = '状態';
+$lang['off']                   = '非表示';
+$lang['open']                  = 'オープン';
+$lang['closed']                = 'クローズ';
+$lang['reply']                 = '返事';
+$lang['replies']               = '返事';
+$lang['newthread']             = '新しいスレッド';
+$lang['toggle_display']        = '表示／非表示';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -1,33 +1,30 @@
 <?php
+
 /**
- * Japanese language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Ikuo Obataya <I.Obataya@gmail.com>
  * @author     Yusuke Ishida <yusuke.ishida.gnl@gmail.com>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
- 
-// for the configuration manager
-$lang['automatic']    = 'デフォルトで全てのページにコメントセクションを追加';
-$lang['allowguests']  = '登録していないユーザーのコメントを許可';
-$lang['showguests']   = 'ゲストのコメント表示を許可';
-$lang['linkemail']    = 'コメントした人の名前にe-mailリンクを貼る';
-$lang['useavatar']    = 'コメントのアバターとしてユーザー画像を使用';
-$lang['urlfield']     = 'ユーザーのURL入力を許可';
-$lang['addressfield'] = 'ユーザーの郵便番号入力を許可';
-$lang['adminimport']  = '管理者にインポートする為の全てのフィールドの設定を許可';
-$lang['usecocomment'] = 'coCommentトラッキングを使う';
-$lang['wikisyntaxok'] = 'コメント内でもwiki文法を許可';
-$lang['subscribe']    = 'コメント購読の有効／無効を表示';
-$lang['newestfirst']  = '新しいコメントを最初に表示';
-$lang['moderate']     = 'コメント内容を管理者が確認する';
-
-$lang['usethreading'] = 'コメントのスレッド表示を許可';
-$lang['userealname']  = 'ユーザー名の代わりにwikiのユーザー情報に登録されている名前を表示する';
-
-$lang['threads_formposition']          = '新しいスレッドフォームの位置';
-$lang['threads_formposition_o_off']    = 'オフ';
-$lang['threads_formposition_o_top']    = '上';
+$lang['automatic']             = 'デフォルトで全てのページにコメントセクションを追加';
+$lang['allowguests']           = '登録していないユーザーのコメントを許可';
+$lang['showguests']            = 'ゲストのコメント表示を許可';
+$lang['linkemail']             = 'コメントした人の名前にe-mailリンクを貼る';
+$lang['useavatar']             = 'コメントのアバターとしてユーザー画像を使用';
+$lang['urlfield']              = 'ユーザーのURL入力を許可';
+$lang['addressfield']          = 'ユーザーの郵便番号入力を許可';
+$lang['adminimport']           = '管理者にインポートする為の全てのフィールドの設定を許可';
+$lang['usecocomment']          = 'coCommentトラッキングを使う';
+$lang['wikisyntaxok']          = 'コメント内でもwiki文法を許可';
+$lang['subscribe']             = 'コメント購読の有効／無効を表示';
+$lang['newestfirst']           = '新しいコメントを最初に表示';
+$lang['moderate']              = 'コメント内容を管理者が確認する';
+$lang['usethreading']          = 'コメントのスレッド表示を許可';
+$lang['userealname']           = 'ユーザー名の代わりにwikiのユーザー情報に登録されている名前を表示する';
+$lang['threads_formposition']  = '新しいスレッドフォームの位置';
+$lang['threads_formposition_o_off'] = 'オフ';
+$lang['threads_formposition_o_top'] = '上';
 $lang['threads_formposition_o_bottom'] = '下';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['visibilityButton']      = 'コメントセクションの表示を切り替えるボタンを有効にする';
+$lang['moderatorgroups']       = '承認者グループ（DokuWikiのマネージャー権限と同じレベル）のカンマ区切り一覧';
