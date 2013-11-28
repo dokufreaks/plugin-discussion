bfe71a9cd92b3d6b8e1c711125ddd2b6348af8bd
Nicolas Charpenel <n.charpenel@laposte.net>
2013-11-28 22:00:41 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,26 +1,31 @@
 <?php
+
 /**
- * French language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Damien Raude-Morvan <drazzib@drazzib.com>
- * @author     Raphael Wils <info@r-wils.fr>
- * @author     Benjamin HKB <http://benjamin.hkb.free.fr>
+ * 
+ * @author Damien Raude-Morvan <drazzib@drazzib.com>
+ * @author Raphael Wils <info@r-wils.fr>
+ * @author Benjamin HKB <http://benjamin.hkb.free.fr>
+ * @author Nicolas Charpenel <n.charpenel@laposte.net>
  */
- 
-// for the configuration manager
-
-$lang['automatic']    = 'Discussion sur toutes les pages par défaut';
-$lang['allowguests']  = 'Permettre les commentaires des utilisateurs non-enregistrés';
-$lang['linkemail']    = 'Mettre un lien vers l\'email de la personne qui commente';
-$lang['useavatar']    = 'Utiliser les avatars pour les commentaires';
-$lang['urlfield']     = 'Affichage d\'un champ URL';
-$lang['addressfield'] = 'Affichage d\'un champ adresse postale';
-$lang['adminimport']  = 'Permettre aux administrateurs de saisir tous les champs pour l\'import';
-$lang['usecocomment'] = 'Utiliser coComment tracking';
-$lang['wikisyntaxok'] = 'Autoriser la syntaxe wiki dans les commentaires';
-$lang['threads_formposition']          = 'Position du champ de saisie des commentaires';
-$lang['threads_formposition_o_top']    = 'haut';
+$lang['automatic']             = 'Discussion sur toutes les pages par défaut';
+$lang['allowguests']           = 'Permettre les commentaires des utilisateurs non-enregistrés';
+$lang['showguests']            = 'autoriser les visiteurs à lire les commentaires';
+$lang['linkemail']             = 'Mettre un lien vers l\'email de la personne qui commente';
+$lang['useavatar']             = 'Utiliser les avatars pour les commentaires';
+$lang['urlfield']              = 'Affichage d\'un champ URL';
+$lang['addressfield']          = 'Affichage d\'un champ adresse postale';
+$lang['adminimport']           = 'Permettre aux administrateurs de saisir tous les champs pour l\'import';
+$lang['usecocomment']          = 'Utiliser coComment tracking';
+$lang['wikisyntaxok']          = 'Autoriser la syntaxe wiki dans les commentaires';
+$lang['subscribe']             = 'activer/désactiver les souscriptions aux commentaires';
+$lang['newestfirst']           = 'lister les commentaires les plus récents en premier';
+$lang['moderate']              = 'modérer les nouveaux commentaires';
+$lang['usethreading']          = 'autoriser les commentaires en arborescence';
+$lang['userealname']           = 'afficher le nom réel plutôt que le nom d\'utilisateur des utilisateurs enregistrés';
+$lang['threads_formposition']  = 'Position du champ de saisie des commentaires';
+$lang['threads_formposition_o_off'] = 'désactivé';
+$lang['threads_formposition_o_top'] = 'haut';
 $lang['threads_formposition_o_bottom'] = 'bas';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['visibilityButton']      = 'Montrer un bouton activant l\'affichage de la section de discussion';
+$lang['moderatorgroups']       = 'Liste séparée par des points-virgules des groupes de moderateurs (équivalent aux niveaux de permission du DokuWiki-Manager)';
