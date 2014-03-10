39edc48f2957483c19fa8ba3828d8e0101374220
Michael Hamann <michael@content-space.de>
2014-03-10 12:35:15 +0100

    Revert pt translation update
    After closer examination the updated translation doesn't seem to be valid and appears to be spam.

--- lang/pt/lang.php
+++ lang/pt/lang.php
@@ -4,12 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Adriano Afonso <mail@adrianoafonso.net>
- * @author Emanuel Penteado Davila <penteado.140@gmail.com>
  */
 $lang['discussion']            = 'Discussão';
 $lang['comment']               = 'Comentário';
 $lang['comments']              = 'Comentários';
-$lang['address']               = 'Rua Sueli Dias Ribeiro';
 $lang['btn_reply']             = 'Responder';
 $lang['btn_hide']              = 'Ocultar';
 $lang['btn_show']              = 'Exibir';
