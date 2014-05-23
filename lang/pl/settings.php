9b8e63f1dba01aa3605bb94b67b45964a0898768
Mati <mackosa@wp.pl>
2014-05-23 07:25:06 +0200

    translation update

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -7,7 +7,14 @@
  * @author Mati <mackosa@wp.pl>
  */
 $lang['allowguests']           = 'Niezarejestrowani mogą komentować';
+$lang['showguests']            = 'pozwól gościom na czytanie komentarzy';
 $lang['linkemail']             = 'Nazwa urzytkownika podlinkowana jego adresem e-mail';
 $lang['useavatar']             = 'Wyświetlaj avatar\'y obok nazwy komentującego';
+$lang['urlfield']              = 'pozwól użytkownikom wejść do URL';
+$lang['addressfield']          = 'pozwól użytkownikom wejść do adresu pocztowego';
+$lang['wikisyntaxok']          = 'pozwól na składnię wiki w komentarzach';
 $lang['newestfirst']           = 'jako pierwsze, wyświetl nowsze komentarze';
 $lang['moderate']              = 'moderuj nowe komentarze';
+$lang['userealname']           = 'wyświetl prawdziwe imię zamiast nazwy zarejestrowanych użytkowników na wiki';
+$lang['threads_formposition_o_top'] = 'góra';
+$lang['threads_formposition_o_bottom'] = 'dół';
