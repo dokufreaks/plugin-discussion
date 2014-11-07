aaa25a9013cf387184d256815040ed1f3c5cad6f
Rafael <ea4bpn@gmail.com>
2014-11-07 17:10:08 +0100

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,29 +1,28 @@
 <?php
+
 /**
- * Spanish language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Jos Antonio Fuentes Santiago <joanfusan@hotmail.com>
- * @author     Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ * 
+ * @author Jos Antonio Fuentes Santiago <joanfusan@hotmail.com>
+ * @author Herman Fabián Sandoval Manrique <hfsandovalm@emzac.com>
+ * @author Rafael <ea4bpn@gmail.com>
  */
- 
-// para el administrador de configuración
-$lang['automatic']        = 'mostrar predeterminada la sección de discusión en cada página';
-$lang['allowguests']      = 'permitir los comentarios a los usuarios no registrados';
-$lang['linkemail']        = 'enlazar el nombre de los comentaristas con su e-mail';
-$lang['usegravatar']      = 'usar un gravatar con imágenes de los usuarios para los comentarios';
-
-$lang['gravatar_size']    = 'tamaño predeterminado para el gravatar';
-$lang['gravatar_rating']  = 'puntuación mínima para elegir gravatars';
-
-$lang['urlfield']        = 'permitir a los usuarios ingresar una URL';
-$lang['addressfield']    = 'permitir a los usuarios ingresar una dirección postal';
-$lang['adminimport']     = 'permitir a los administradores definir todos los campos a insertar';
-$lang['usecocomment']    = 'usar seguimiento de comentarios coComment';
-$lang['wikisyntaxok']    = 'permitir sintaxis wiki en comentarios';
-
-$lang['threads_formposition']          = 'posición del formulario del nuevo post';
-$lang['threads_formposition_o_top']    = 'superior';
+$lang['automatic']             = 'mostrar predeterminada la sección de discusión en cada página';
+$lang['allowguests']           = 'permitir los comentarios a los usuarios no registrados';
+$lang['showguests']            = 'permitir a los invitados leer comentarios';
+$lang['linkemail']             = 'enlazar el nombre de los comentaristas con su e-mail';
+$lang['useavatar']             = 'usar avatar del usuario para los comentarios';
+$lang['urlfield']              = 'permitir a los usuarios ingresar una URL';
+$lang['addressfield']          = 'permitir a los usuarios ingresar una dirección postal';
+$lang['adminimport']           = 'permitir a los administradores definir todos los campos a insertar';
+$lang['usecocomment']          = 'usar seguimiento de comentarios coComment';
+$lang['wikisyntaxok']          = 'permitir sintaxis wiki en comentarios';
+$lang['subscribe']             = 'activar/desactivar la suscripción a comentarios';
+$lang['newestfirst']           = 'listar nuevos comentarios primero';
+$lang['moderate']              = 'moderar nuevos comentarios';
+$lang['userealname']           = 'mostrar nombre real en lugar del nombre de usuario registrado';
+$lang['threads_formposition']  = 'posición del formulario del nuevo post';
+$lang['threads_formposition_o_top'] = 'superior';
 $lang['threads_formposition_o_bottom'] = 'inferior';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['visibilityButton']      = 'Activar botón de visibilidad de la sección de discussion';
+$lang['moderatorgroups']       = 'Lista separada por comas de grupos de moderadores (lo mismo que el nivel de permisos en el gestor de DokuWiki)';
