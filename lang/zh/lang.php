640969926b86be1e99f822b0d7f897b45280d8ed
Peter Dicosta <peterdicosta17@gmail.com>
2015-04-09 12:50:18 +0200

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -1,49 +1,39 @@
 <?php
+
 /**
- * Chinese Simplified language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     ZDYX <zhangduyixiong@gmail.com>
+ * 
+ * @author ZDYX <zhangduyixiong@gmail.com>
  */
-
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = '待审核的评论';
-
-$lang['discussion']      = '评论'; //新版已经修正TOC 问题，翻译之。
-$lang['comment']         = '评论';
-$lang['comments']        = '评论';
-$lang['nocomments']      = '无评论';
-$lang['url']             = '个人主页';
-$lang['address']         = '通信地址';
-$lang['date']            = '评论日期';
-$lang['entercomment']    = '请输入您的评论';
-$lang['wikisyntax']      = '可以使用维基语法';
-$lang['older']           = '更早的评论';
-$lang['newer']           = '更晚的评论';
-$lang['btn_reply']       = '回复';
-$lang['btn_hide']        = '隐藏';
-$lang['btn_show']        = '显示';
-$lang['btn_change']      = '修改';
-$lang['statuschanged']   = '讨论状态变化了';
-$lang['wordblock']       = '您的评论由于类似垃圾评论而被阻止发表。';
-$lang['moderation']      = '你的评论已添加并且等待审核。';
-$lang['mail_newcomment'] = '新评论';
-$lang['subscribe']       = '订阅评论';
-$lang['noguests']        = '你需要登录发表评论。';
-
-$lang['status']          = '状态';
-$lang['off']             = '关';
-$lang['open']            = '开';
-$lang['closed']          = '关闭';
-
-$lang['reply']           = '条回复';
-$lang['replies']         = '条回复';
-$lang['newthread']       = '新讨论主题';
-$lang['toggle_display']  = '隐藏/显示评论';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = '待审核的评论';
+$lang['discussion']            = '评论';
+$lang['comment']               = '评论';
+$lang['comments']              = '评论';
+$lang['nocomments']            = '无评论';
+$lang['url']                   = '个人主页';
+$lang['address']               = '通信地址';
+$lang['date']                  = '评论日期';
+$lang['entercomment']          = '请输入您的评论';
+$lang['wikisyntax']            = '可以使用维基语法';
+$lang['older']                 = '更早的评论';
+$lang['newer']                 = '更晚的评论';
+$lang['btn_reply']             = '回复';
+$lang['btn_hide']              = '隐藏';
+$lang['btn_show']              = '显示';
+$lang['btn_change']            = '修改';
+$lang['statuschanged']         = '讨论状态变化了';
+$lang['wordblock']             = '您的评论由于类似垃圾评论而被阻止发表。';
+$lang['moderation']            = '你的评论已添加并且等待审核。';
+$lang['mail_newcomment']       = '新评论';
+$lang['subscribe']             = '订阅评论';
+$lang['noguests']              = '你需要登录发表评论。';
+$lang['status']                = '状态';
+$lang['off']                   = '关';
+$lang['open']                  = '开';
+$lang['closed']                = '关闭';
+$lang['reply']                 = '条回复';
+$lang['replies']               = '条回复';
+$lang['newthread']             = '新讨论主题';
+$lang['toggle_display']        = '隐藏/显示评论';
