700f566184b2c2567ef3c757da02305c1e74679f
Danny Buckhof <daniel.raknes@hotmail.no>
2015-05-07 14:35:07 +0200

    translation update

---
+++ lang/no/lang.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Danny Buckhof <daniel.raknes@hotmail.no>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['discussion']            = 'Diskusjon';
+$lang['comment']               = 'Kommentar';
+$lang['comments']              = 'Kommentarer';
+$lang['nocomments']            = 'Kommentarer';
+$lang['threads']               = 'Diskusjoner';
+$lang['url']                   = 'Webside';
+$lang['address']               = 'Adresse';
+$lang['date']                  = 'Dato';
+$lang['entercomment']          = 'Sett inn din kommentar';
+$lang['wikisyntax']            = 'Wiki syntax er tillatt';
+$lang['older']                 = 'Eldre diskusjoner';
+$lang['newer']                 = 'Nyere diskusjoner';
+$lang['btn_reply']             = 'Svar';
+$lang['btn_hide']              = 'Gjem';
+$lang['btn_show']              = 'Vis';
+$lang['btn_change']            = 'Endre';
+$lang['statuschanged']         = 'Diskusjonsstatus endret';
+$lang['wordblock']             = 'Din kommentar ble avvist som spam';
+$lang['moderation']            = 'Din kommentar er lagt inn og avventer godkjenning';
+$lang['mail_newcomment']       = 'Ny kommentar';
+$lang['subscribe']             = 'Abonner på kommentarer';
+$lang['noguests']              = 'Du kan legge inn en kommentar hvis du er innlogget.';
+$lang['status']                = 'Status';
+$lang['off']                   = 'av';
+$lang['open']                  = 'åpen';
+$lang['closed']                = 'lukket';
+$lang['reply']                 = 'Svar';
+$lang['replies']               = 'Flere svar';
+$lang['newthread']             = 'Ny diskusjonsside';
+$lang['toggle_display']        = 'Gjem/Vis';
---
+++ lang/no/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Danny Buckhof <daniel.raknes@hotmail.no>
+ */
+$lang['automatic']             = 'diskusjonsmodul på hver side er standard';
+$lang['allowguests']           = 'tillat kommentarer fra uregistrerte brukere';
+$lang['showguests']            = 'tillat gjester å lese kommentarer';
+$lang['linkemail']             = 'link kommentererens navn med e-post';
+$lang['useavatar']             = 'bruk avatar brukerbilder for kommentarer';
+$lang['urlfield']              = 'tillat brukere å legge inn URL';
+$lang['addressfield']          = 'tillat brukere å legge inn postadresse';
+$lang['adminimport']           = 'tillat administratorer å sette alle felt for import';
+$lang['usecocomment']          = 'bruk coComment kommentarsporing';
+$lang['wikisyntaxok']          = 'tillat wiki syntax i kommentarer';
+$lang['subscribe']             = 'aktiver/deaktiver abonnering av kommentarer';
+$lang['newestfirst']           = 'nyeste kommentarer øverst';
+$lang['moderate']              = 'godkjenn nye kommentarer';
+$lang['usethreading']          = 'tillat tråd for kommentarer';
+$lang['userealname']           = 'vis opprinnelig navn istedenfor brukernavn for registrerte brukere';
+$lang['threads_formposition_o_off'] = 'av';
+$lang['threads_formposition_o_top'] = 'topp';
+$lang['threads_formposition_o_bottom'] = 'bunn';
+$lang['visibilityButton']      = 'Aktiver knapp for å bytte visningen av diskusjonsmodulen';
