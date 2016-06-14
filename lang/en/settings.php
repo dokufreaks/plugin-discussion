5cb5a67fa44caaac34437613785a257b824cefa6
Smithc281 <smithc586@gmail.com>
2016-06-14 11:30:36 +0200

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,36 +1,30 @@
 <?php
+
 /**
- * English language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
+ *
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author Smithc281 <smithc586@gmail.com>
  */
- 
-// for the configuration manager
-$lang['automatic']    = 'discussion section on every page by default';
-$lang['excluded_ns']  = 'list of namespaces to revert the option above (a regular expression e.g. <code>/(playground|wiki)/</code> )';
-$lang['allowguests']  = 'allow comments by unregistered users';
-$lang['showguests']   = 'allow guests to read comments';
-$lang['linkemail']    = 'link commenters name with e-mail';
-$lang['useavatar']    = 'use avatar user images for comments';
-$lang['urlfield']     = 'allow users to enter an URL';
-$lang['addressfield'] = 'allow users to enter a postal address';
-$lang['adminimport']  = 'allow admins to set  all fields for import';
-$lang['wikisyntaxok'] = 'allow wiki syntax in comments';
-$lang['subscribe']    = 'enable/disable subscribe to comments';
-$lang['newestfirst']  = 'list newest comments first';
-$lang['moderate']     = 'moderate new comments';
-
-$lang['usethreading'] = 'allow threading of comments';
-$lang['userealname']  = 'display real name instead of user name of registered wiki users';
-
-$lang['threads_formposition']          = 'position of the new thread form shown by the <code>{{threads> }}</code> syntax';
-$lang['threads_formposition_o_off']    = 'off';
-$lang['threads_formposition_o_top']    = 'top';
-$lang['threads_formposition_o_bottom'] = 'bottom';
-
-$lang['visibilityButton'] = 'Enable button for toggling the visibility of the discussion section';
-
-$lang['moderatorgroups']  = 'Comma-separated list of moderator groups (equal to DokuWiki-Manager permission level)';
-$lang['moderatorsnotify']  = 'Send notification emails of the new comments to moderators';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['automatic']             = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['excluded_ns']           = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['allowguests']           = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['showguests']            = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['linkemail']             = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['useavatar']             = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['urlfield']              = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['addressfield']          = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['adminimport']           = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['wikisyntaxok']          = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['subscribe']             = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['newestfirst']           = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['moderate']              = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['usethreading']          = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['userealname']           = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['threads_formposition']  = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['threads_formposition_o_off'] = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['threads_formposition_o_top'] = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['threads_formposition_o_bottom'] = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['visibilityButton']      = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['moderatorgroups']       = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
+$lang['moderatorsnotify']      = 'In My Bambino site you can find sso many interesting aspefts eddadcgekkkdfeff';
