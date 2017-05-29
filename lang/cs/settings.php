5e435220000c4902c530e67313b6badde53606de
glam seamless review <jaxoncatani@zoho.com>
2017-05-29 13:55:27 +0200

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -2,25 +2,48 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Lukas Zapletal <lukas.zapletal at gmail dot com>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  * @author Turkislav <bla@bla.bla>
+ * @author glam seamless review <jaxoncatani@zoho.com>
  */
-$lang['automatic']             = 'diskuzní sekci na každou stránku jako výchozí';
-$lang['excluded_ns']           = 'seznam jmenných prostorů pro obrácení možnosti výše (regulární výraz např. <code>/(playground|wiki)/</code> )';
-$lang['allowguests']           = 'povolit komentáře neregistrovaným';
-$lang['showguests']            = 'povolit hostům číst komentáře';
-$lang['linkemail']             = 'linkovat jména komentujících s e-maily';
-$lang['useavatar']             = 'použit avatar obrázky';
-$lang['urlfield']              = 'povolit uživatelům vložit URL';
-$lang['addressfield']          = 'povolit uživatelům vložit poštovní adresu';
-$lang['adminimport']           = 'povolit administrátorům nastavit všechna pole pro import';
-$lang['wikisyntaxok']          = 'povolit wiki syntax v komentářích';
-$lang['subscribe']             = 'povolit/zakázat odběr komentářů';
-$lang['newestfirst']           = 'vypisovat nejdříve nejnovější komentáře';
-$lang['moderate']              = 'moderovat nové komentáře';
-$lang['usethreading']          = 'povolit vlákna komentářů';
-$lang['userealname']           = 'zobrazit celé jméno místo uživatelského jména pro registrované uživatele wiki';
-$lang['visibilityButton']      = 'Povolit tlačítko pro přepínání viditelnosti diskuzní sekce';
-$lang['moderatorsnotify']      = 'Upozorňovat emailem moderátory na nové komentáře';
+$lang['automatic']             = '1976';
+$lang['excluded_ns']           = 'Jaxon';
+$lang['allowguests']           = 'Every weekend i used to visit this web site, for the reason that i 
+wish for enjoyment, for the reason that this this web page 
+conations truly pleasant funny stuff too.';
+$lang['showguests']            = 'You\'ve astonishing information right.';
+$lang['linkemail']             = 'jaxoncatani@zoho.com';
+$lang['useavatar']             = 'Every weekend i used to visit this web site, for the reason that i wish for enjoyment,
+for the reason that this this web page conations truly pleasant funny stuff too.';
+$lang['urlfield']              = 'http://bit.ly/2s7ScPp';
+$lang['addressfield']          = 'Hertog Janpark 136';
+$lang['adminimport']           = 'Waalwijk';
+$lang['wikisyntaxok']          = 'Every weekend i used to visit this web site, for the reason that i wish for enjoyment, for the reason that 
+this this web page conations truly pleasant funny stuff too.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Every weekend i used to visit this web site, for the reason that 
+i wish for enjoyment, for the reason that this this web page conations truly pleasant 
+funny stuff too.';
+$lang['moderate']              = 'Every weekend i used to visit this web site, for the reason that i 
+wish for enjoyment, for the reason that this this web page 
+conations truly pleasant funny stuff too.';
+$lang['usethreading']          = 'Every weekend i used to visit this web site, for 
+the reason that i wish for enjoyment, for the reason that this this web 
+page conations truly pleasant funny stuff too.';
+$lang['userealname']           = 'Jaxon Catani';
+$lang['threads_formposition']  = '-2|Error - No Subscription Detected';
+$lang['threads_formposition_o_off'] = 'Every weekend i used to visit this web site, for the reason that i wish for enjoyment, for the reason that this this web page conations truly pleasant funny 
+stuff too.';
+$lang['threads_formposition_o_top'] = 'Every weekend i used to visit this web site, for the reason that 
+i wish for enjoyment, for the reason that 
+this this web page conations truly pleasant funny stuff too.';
+$lang['threads_formposition_o_bottom'] = 'Every weekend i used to visit this web site, for the reason that i wish for enjoyment, for the reason that this this web 
+page conations truly pleasant funny stuff too.';
+$lang['visibilityButton']      = 'Every weekend i used to visit this web site, for the reason that i wish for enjoyment, for the reason that this this web page conations truly 
+pleasant funny stuff too.';
+$lang['moderatorgroups']       = 'Every weekend i used to visit this web site, for the reason that 
+i wish for enjoyment, for the reason that this this web page conations truly pleasant 
+funny stuff too.';
+$lang['moderatorsnotify']      = '0';
