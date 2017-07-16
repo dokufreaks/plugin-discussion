141a866162a6c61ce0fab086bb3d8fd5983de060
Tor Härnqvist <tor@harnqvist.se>
2017-07-16 16:25:12 +0200

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['automatic']             = 'Diskussionssektion på varje sida som standard';
+$lang['excluded_ns']           = 'lista på namnrymder för att revertera alternativet ovan (ett reguljärt uttryck t ex <code>/(playground|wiki)/</code> )';
+$lang['allowguests']           = 'tillåt kommentarer av oregistrerade besökare';
+$lang['showguests']            = 'tillåt besökare att läsa kommentarer';
+$lang['linkemail']             = 'Länka kommentarslämnarens namn med e-post';
+$lang['useavatar']             = 'använd avatar-användarbilder för kommentarer';
+$lang['urlfield']              = 'tillåt användare att fylla i URL';
+$lang['addressfield']          = 'tillåt användare att fylla i postadress';
+$lang['adminimport']           = 'tillåt administratörer att välja alla fält för importering';
+$lang['wikisyntaxok']          = 'tillåt wiki-syntax i kommentarer';
+$lang['subscribe']             = 'aktivera/avaktivera prenumeration på kommentarer';
+$lang['newestfirst']           = 'lista senaste kommentarer först';
+$lang['moderate']              = 'godkänn nya kommentarer';
+$lang['usethreading']          = 'tillåt trådning av kommentarer';
+$lang['userealname']           = 'visa riktigt namn istället för användarna på registrerade wiki-användare';
+$lang['threads_formposition']  = 'placera formuläret för nya diskussioner visat med <code>{{threads> }}</code> syntax';
+$lang['threads_formposition_o_off'] = 'av';
+$lang['threads_formposition_o_top'] = 'högst upp';
+$lang['threads_formposition_o_bottom'] = 'längst ner';
+$lang['visibilityButton']      = 'Aktivera knapp för att slå på/av visning av kommentarssektionen';
+$lang['moderatorgroups']       = 'Kommaseparerad lista på moderatorsgrupper (motsvarande behörighetsnivå för DokuWiki-manager)';
+$lang['moderatorsnotify']      = 'Skicka e-brev till moderatorer för notifiering om nya kommentarer';
