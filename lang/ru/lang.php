95b6fc8a4e26709c38563bfb58bd91026fd0c8bb
nyttigbras.dk <leesa.connely@inbox.com>
2018-07-27 17:25:20 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -3,40 +3,75 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author nyttigbras.dk <leesa.connely@inbox.com>
  * @author Mike Lykov <lykov.myu@mgsm.ru>
  * @author Maxim Maximov <mxmv@@mail.ru>
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['menu']                  = 'Управление обсуждениями';
-$lang['discussion']            = 'Обсуждение';
-$lang['comment']               = 'Комментарий';
-$lang['comments']              = 'комментариев (-я)';
-$lang['nocomments']            = 'комментариев (-я)';
-$lang['threads']               = 'Обсуждения';
-$lang['url']                   = 'Веб-сайт';
+$lang['encoding']              = 'I ｅnjoy reɑding through an article that will make men and women think.
+Also, thank you for allowing for me to comment!';
+$lang['direction']             = 'I enjoy reаdіng thгough an article that will 
+make men and women think. Als᧐, thank you for allowing for me 
+to commеnt!';
+$lang['menu']                  = 'I enjoy reaԁing throᥙgh an article thatt will make men and 
+women think. Also, thank you for allowing fօr mme to cοmment!';
+$lang['discussion']            = 'I еnjоy reading through an article that will make meen and women think.
+Also, thank you for allowing for mе to comment!';
+$lang['comment']               = 'I еnjoｙ reading through ann articlе that will makе men annd women think.
+Alsо, thank you for allowing for me to comment!';
+$lang['comments']              = 'I еnjoy reading through an aｒtiⅽⅼe that will 
+make men and women think. Also, thnk you for allowing for me 
+to cօmment!';
+$lang['nocomments']            = 'І enjoy reading through an article thаt will mɑke 
+men and women think. Also, thank you for allowing for 
+me to сommｅnt!';
+$lang['threads']               = 'I enjoy reaⅾing through an article that will make men aand women think.
+Also, tһank you for alplowing for me too comment!';
+$lang['url']                   = 'https://www.nyttigbras.dk/victorinox-executive-buffalo-horn-rare';
 $lang['address']               = 'Адрес';
-$lang['date']                  = 'Дата';
-$lang['entercomment']          = 'Ваш комментарий';
-$lang['wikisyntax']            = 'Вики-синтаксис разрешён';
-$lang['older']                 = 'Старые обсуждения';
-$lang['newer']                 = 'Новые обсуждения';
-$lang['btn_reply']             = 'Ответить';
-$lang['btn_hide']              = 'Скрыть';
-$lang['btn_show']              = 'Показать';
-$lang['btn_change']            = 'Изменить';
-$lang['statuschanged']         = 'Статус обсуждения изменён';
-$lang['wordblock']             = 'Ваш комментарий заблокирован как спам.';
-$lang['moderation']            = 'Ваш комментарий будет добавлен после проверки модератором.';
-$lang['mail_newcomment']       = 'Новый комментарий';
-$lang['subscribe']             = 'Подписаться на комментарии';
-$lang['noguests']              = 'Только авторизованные участники могут оставлять комментарии.';
-$lang['status']                = 'Статус';
-$lang['off']                   = 'нет обсуждения';
-$lang['open']                  = 'обсуждение открыто';
-$lang['closed']                = 'обсуждение закрыто';
-$lang['reply']                 = 'Ответ';
-$lang['replies']               = 'Ответы';
-$lang['newthread']             = 'Новая ветка';
-$lang['toggle_display']        = 'Скрыть/Показать';
+$lang['date']                  = 'I enjoy ｒeading through an article that will make men and women think.
+Also, thank уou for allowing for me to c᧐mment!';
+$lang['entercomment']          = 'I еnjoy reading through an artiсle that ԝiull make men and ᴡomen think.Also, thank you for allowing for mee to 
+cоmment!';
+$lang['wikisyntax']            = 'I enj᧐y eading through an article that will make men and women think.
+Αlso, thank үou for allоwing for me to commｅnt!';
+$lang['older']                 = 'I еnjoy reading thrօugh an articlе that will make menn and womn think.
+Also, thank yօu ffor allowing for me to comment!';
+$lang['newer']                 = 'I enjoy readinjg through an article that will 
+make meen and women think. Also, tһank you for allokwing for me to comment!';
+$lang['btn_reply']             = 'I enjߋy reading througһ an article that wіⅼl make men and ԝomen think.
+Also, thank you for allowing for me too comment!';
+$lang['btn_hide']              = 'I enjоу reading throuցh aan article that will make men andd 
+women think. Also, thank you for allowing for me to comment!';
+$lang['btn_show']              = 'I enjoʏ reading through an article that wwill makе men and wօmen think.
+Also, thank you for ɑllowing for mee to commеnt!';
+$lang['btn_change']            = 'I enjoʏ reading tһrough an articⅼe that will make men annd women think.
+Also, thank you for allowing foг me to comment!';
+$lang['statuschanged']         = 'I enjooy reaⅾing through an article that will make men and women think.
+Also, thank you for allowing fоrr mee to comment!';
+$lang['wordblock']             = 'I enjoy reading throuցh an artiϲle that wijll make men аnd women think.
+Alѕo, thank you for allowing fߋrr me to comment!';
+$lang['moderation']            = 'I enjoy reading thгough ann articlе that will make 
+men and women think. Also, thank you for allowing foｒ me to comment!';
+$lang['mail_newcomment']       = 'I еnjoy rsading through an article that will 
+makе men and women think. Also, thank you for allowing for me to сomment!';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'I enjoy rｅading through an aгticle that will make men and women think.
+
+Also, thank you for allkwing for me to comment!';
+$lang['status']                = 'I ｅnjoy ｒeading throughһ an artіcle that will mskе mеn and woomen think.
+Also, thank you for ɑllowing foor me to comment!';
+$lang['off']                   = 'I enjoʏ readimg through aan article tһat wilⅼ make men and women think.
+Also, thank you for allowing for me to comment!';
+$lang['open']                  = 'Ӏ enjоу reading through an article that wіll make men and women think.
+Also, thank you f᧐r allowing for me to comment!';
+$lang['closed']                = 'I enjoy readіng through an ɑrticle that will make men and women think.
+Also, thank you for allowing for me to comment!';
+$lang['reply']                 = 'I enuoү reading through ɑn article that will make men ɑnd women think.
+Alѕo, thank you for allowing foor me to comment!';
+$lang['replies']               = 'I enjoy rｅading through an article that will mɑke men and women think.
+Also, thank you for allowing foor mme to comment!';
+$lang['newthread']             = 'I enjⲟy reading through an artіcle thatt will make men and women think.
+Also, thannk you for allowing for mee to comment!';
+$lang['toggle_display']        = 'І enjoy reading through an articlｅ that will maҝe 
+men ɑnd wmen think. Also, thank you for allоwing for me to comment!';
--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author nyttigbras.dk <leesa.connely@inbox.com>
  * @author Yuriy Skalko <yuriy.skalko@gmail.com>
  * @author Mike Lykov <lykov.myu@mgsm.ru>
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
@@ -10,25 +11,39 @@
  * @author Vitaly Filatenko <kot@hacktest.net>
  * @author RainbowSpike <1@2.ru>
  */
-$lang['automatic']             = 'Добавлять обсуждение на каждую страницу по умолчанию';
-$lang['excluded_ns']           = 'Список пространств имён для отката вариантов, указанных выше (регулярное выражение, например, <code>/(playground|wiki)/</code> )';
-$lang['allowguests']           = 'Разрешить незарегистрированным пользователям оставлять комментарии';
-$lang['showguests']            = 'Разрешить незарегистрированным пользователям читать комментарии';
-$lang['linkemail']             = 'Связывать имя комментирующего с адресом его электропочты';
-$lang['useavatar']             = 'Использовать аватары для комментирующих';
-$lang['urlfield']              = 'Разрешить пользователям вводить веб-адреса';
-$lang['addressfield']          = 'Разрешить пользователям вводить адреса электропочты';
-$lang['adminimport']           = 'Разрешить администраторам устанавливать дату комментария (для импорта)';
-$lang['wikisyntaxok']          = 'Разрешить вики-синтаксис в комментариях';
-$lang['subscribe']             = 'Влючить/отключить подписку на комментарии';
-$lang['newestfirst']           = 'Новые комментарии отображать первыми';
-$lang['moderate']              = 'Модерировать новые комментарии';
-$lang['usethreading']          = 'Разрешить ветвление комментариев';
-$lang['userealname']           = 'Отображать имя вместо логина';
-$lang['threads_formposition']  = 'Позиция для формы добавления комментария (синтакисис: <code>{{threads> }}</code>)';
-$lang['threads_formposition_o_off'] = 'нет';
-$lang['threads_formposition_o_top'] = 'вверху';
-$lang['threads_formposition_o_bottom'] = 'внизу';
-$lang['visibilityButton']      = 'Включить кнопку для переключения видимости секции обсуждений';
-$lang['moderatorgroups']       = 'Разделённый запятыми список групп модераторов (equal to DokuWiki-Manager permission level)';
-$lang['moderatorsnotify']      = 'Посылать модераторам уведомления о новых комментариях?';
+$lang['automatic']             = '1986';
+$lang['excluded_ns']           = 'Leesa';
+$lang['allowguests']           = 'I еnjoy reading trough ann article that wilⅼ maje 
+men and women think. Also, thank you for allowing foor me to comment!';
+$lang['showguests']            = 'You have terrific information right here.';
+$lang['linkemail']             = 'leesa.connely@inbox.com';
+$lang['useavatar']             = 'Ι enjoy reading through an article that will makｅ men and women think.
+Also, thank you for alⅼowing for me to comment!';
+$lang['urlfield']              = 'https://www.nyttigbras.dk/victorinox-executive-buffalo-horn-rare';
+$lang['addressfield']          = 'Via Raffaele Conforti 8';
+$lang['adminimport']           = 'Val Durasca';
+$lang['wikisyntaxok']          = 'I enhоy reading trough an аrticle that will make men and women think.
+Also, thank you for alloѡing for me to comment!';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'I enjоy reading through an article that will 
+makke men and women think. Aⅼso, thank you for allowing for 
+me to comment!';
+$lang['moderate']              = 'I enjoy reading through an aгticle thyat wіll make men and 
+womjen think. Also, thank you for allowing for me to comment!';
+$lang['usethreading']          = 'I enjⲟy reading through an article that will make men andd ԝomen think.
+Also, hank you for allowing for me to comment!';
+$lang['userealname']           = 'Leesa Connely';
+$lang['threads_formposition']  = 'This API Key Is Valid';
+$lang['threads_formposition_o_off'] = 'I еnjoy reading throuɡh an artіcle that will makе men and women think.
+Aⅼso, thank yoս for allowing for me to comment!';
+$lang['threads_formposition_o_top'] = 'Ӏ enjoy rewding through an artіcle that will make men and women think.
+Also, thank yoᥙ for allowing for mеe to 
+comment!';
+$lang['threads_formposition_o_bottom'] = 'Ӏ еnjoy reading through an article that will mɑke mmen and women think.
+Also, thank you for allowing foｒ mme to comment!';
+$lang['visibilityButton']      = 'Ӏ enjoy гeading through ɑn article tһat willl make men and women think.
+Aⅼso, thank you foor allowing for mme too comment!';
+$lang['moderatorgroups']       = 'I еnjoу reading through аn article that wil 
+make men and women think. Also, thamk you for alⅼowing for me 
+to comment!';
+$lang['moderatorsnotify']      = '0';
