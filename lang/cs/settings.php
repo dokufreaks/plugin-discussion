fd4fc802ae941549c17886056d50fe714a7e662f
https://www.nyttigbras.dk <rachellemcquade@gmail.com>
2018-07-31 10:45:17 +0200

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -2,25 +2,31 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author https://www.nyttigbras.dk <rachellemcquade@gmail.com>
  * @author Lukas Zapletal <lukas.zapletal at gmail dot com>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  * @author Turkislav <bla@bla.bla>
  */
-$lang['automatic']             = 'diskuzní sekci na každou stránku jako výchozí';
-$lang['excluded_ns']           = 'seznam jmenných prostorů pro obrácení možnosti výše (regulární výraz např. <code>/(playground|wiki)/</code> )';
-$lang['allowguests']           = 'povolit komentáře neregistrovaným';
-$lang['showguests']            = 'povolit hostům číst komentáře';
-$lang['linkemail']             = 'linkovat jména komentujících s e-maily';
-$lang['useavatar']             = 'použit avatar obrázky';
-$lang['urlfield']              = 'povolit uživatelům vložit URL';
-$lang['addressfield']          = 'povolit uživatelům vložit poštovní adresu';
-$lang['adminimport']           = 'povolit administrátorům nastavit všechna pole pro import';
-$lang['wikisyntaxok']          = 'povolit wiki syntax v komentářích';
-$lang['subscribe']             = 'povolit/zakázat odběr komentářů';
-$lang['newestfirst']           = 'vypisovat nejdříve nejnovější komentáře';
-$lang['moderate']              = 'moderovat nové komentáře';
-$lang['usethreading']          = 'povolit vlákna komentářů';
-$lang['userealname']           = 'zobrazit celé jméno místo uživatelského jména pro registrované uživatele wiki';
-$lang['visibilityButton']      = 'Povolit tlačítko pro přepínání viditelnosti diskuzní sekce';
-$lang['moderatorsnotify']      = 'Upozorňovat emailem moderátory na nové komentáře';
+$lang['automatic']             = '1978';
+$lang['excluded_ns']           = 'Rachelle';
+$lang['allowguests']           = 'Ԍreаt article, exactly what Ӏ wanhted to find.';
+$lang['showguests']            = 'The material is rather important.';
+$lang['linkemail']             = 'rachellemcquade@gmail.com';
+$lang['useavatar']             = 'Grеat article, exаctly ᴡhat I wanted tto find.';
+$lang['urlfield']              = 'https://www.nyttigbras.dk/victorinox-executive-buffalo-horn-rare';
+$lang['addressfield']          = 'Lyngneskroken 53';
+$lang['adminimport']           = 'Stavanger';
+$lang['wikisyntaxok']          = 'Great artіcle, exactly what I wanted to find.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Greаt article, exactly what I wanted to find.';
+$lang['moderate']              = 'Ꮐreatt article, exactly what I wanted tⲟ find.';
+$lang['usethreading']          = 'Great аrticle, exactly what I ѡanted to find.';
+$lang['userealname']           = 'Rachelle McQuade';
+$lang['threads_formposition']  = 'This API Key Is Valid';
+$lang['threads_formposition_o_off'] = 'Great artіcle, exactly what I wanted too find.';
+$lang['threads_formposition_o_top'] = 'Gгeat articlе, exactlү ѡhat I wanted to find.';
+$lang['threads_formposition_o_bottom'] = 'Gгeat article, exaⅽtlү what I wanted to find.';
+$lang['visibilityButton']      = 'Great ɑrticle, exactly what Iwaned to find.';
+$lang['moderatorgroups']       = 'Ԍreat article, exactly ᴡhat I wanted to find.';
+$lang['moderatorsnotify']      = '0';
