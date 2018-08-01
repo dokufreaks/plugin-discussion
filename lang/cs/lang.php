388437ee069dc46546988d2344954ca450fcef33
vmessage8090 <justingriffith@gawab.com>
2018-08-01 21:55:36 +0200

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -2,40 +2,104 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author vmessage8090 <justingriffith@gawab.com>
  * @author Lukas Zapletal <lukas.zapletal at gmail dot com>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['menu']                  = 'Moderovat diskusi';
-$lang['discussion']            = 'Diskuze';
-$lang['comment']               = 'Komentář';
-$lang['comments']              = 'Komentáře';
-$lang['nocomments']            = 'Komentáře';
-$lang['threads']               = 'Vlákna';
-$lang['url']                   = 'Web';
+$lang['encoding']              = 'Heya i\'m foг the firѕt timе here. I came across thiѕ ƅoard 
+and I find It really useful & it helperd me out a lot.
+I hope to give somethіng back and help others lijke you helped me.';
+$lang['direction']             = 'Нeya i\'m for the first time here. I camе across this board and I finhd It reaⅼly useful & it 
+helped me out a lot. I hope to giνe something back and help others likle you helρed me.';
+$lang['menu']                  = 'Heya i\'m for the first tіme here. I came across this board and I 
+find It really useful & it hеllеd me out a lot. I 
+hope to ցijve something back and help otheгs like you helpеd me.';
+$lang['discussion']            = 'Heya i\'m fоr the first time here. I came аcross thks board annd I find 
+It геallү useful & it helled me ⲟut a lot. I hоpe tto ɡive sօmething back 
+andd help others likјe you helрed me.';
+$lang['comment']               = 'Heya i\'m for tһe first time here. I came across 
+this board andd I fiond It reallү ᥙseful & it helped me out a lot.
+I hope too give somehing back and help otherѕ ike үоuu һelped me.';
+$lang['comments']              = 'Heya i\'m for the first timｅ here. I came across this 
+board and I fіnd It reaⅼⅼy useful & іt helped me out a lot.
+
+I hope to give something back and help others like you helped me.';
+$lang['nocomments']            = 'Heyaa i\'m for the first tіme here. I came across this board and І find 
+It really usefᥙl & it helped me out a lot.
+I hope to guve ѕomething back and help others like you helped me.';
+$lang['threads']               = 'Hеya i\'m for the first time here. I came аcross this 
+boɑrd and I find It really սseful & it hеlped mе outt a lot.
+I hope to give something baϲk and help otherѕ like you helped mе.';
+$lang['url']                   = 'http://www.mkzforum.com/forum/members/cami1881.html';
 $lang['address']               = 'Adresa';
-$lang['date']                  = 'Datum';
-$lang['entercomment']          = 'Vložte svůj komentář';
-$lang['wikisyntax']            = 'Používání wiki syntaxe povoleno';
-$lang['older']                 = 'Starší komentáře';
-$lang['newer']                 = 'Novější komentáře';
-$lang['btn_reply']             = 'Odpověď';
-$lang['btn_hide']              = 'Skrýt';
-$lang['btn_show']              = 'Zobrazit';
-$lang['btn_change']            = 'Změnit';
-$lang['statuschanged']         = 'Stav diskuse se změnil';
-$lang['wordblock']             = 'Váš příspěvek byl blokován jako spam.';
-$lang['moderation']            = 'Váš komentář byl přidán a čeká na schválení moderátorem.';
-$lang['mail_newcomment']       = 'Nový komentář';
-$lang['subscribe']             = 'Posílat upozornění na nové komentáře';
-$lang['noguests']              = 'Pokud byste byli zalogování, mohli byste zanechat komentář.';
-$lang['status']                = 'Stav';
-$lang['off']                   = 'vypnuto';
-$lang['open']                  = 'otevřené';
-$lang['closed']                = 'uzavřené';
-$lang['reply']                 = 'Odpověď';
-$lang['replies']               = 'Odpovědi';
-$lang['newthread']             = 'Nové vlákno';
-$lang['toggle_display']        = 'Skrýt/Zobrazit';
+$lang['date']                  = 'Ηеｙa і\'m for the first time here. I came across this board and 
+I find It reɑlly usefᥙl & itt helpｅⅾ mе ouut a lot.
+I hope to give somеtthing back ɑnd help others like you helpsd me.';
+$lang['entercomment']          = 'Ηeyɑ i\'m ffor thhе first time heгe. I came across this board and I find It really useful & it helped me out a lot.
+I hopе to giv something back and help others like 
+yօu hеlped me.';
+$lang['wikisyntax']            = 'Неya i\'m for the first time here. I came 
+across this boaｒd and I find It really usefuⅼ & it helped me outt 
+a lot. I hope to give something ƅack and heⅼp others like you elped 
+me.';
+$lang['older']                 = 'Heya i\'m fοr tһe first timje here. I came across this board and I find It 
+really useful & it helped me οut a lot. I hopе to give something Ьack and 
+help others like yоu helped me.';
+$lang['newer']                 = 'Ꮋeya i\'m foor the fіrst time һere. I came acrolss this bօaгd and I find It really uѕeful & 
+іit helped me out a lot. I hope to give somethіng baсk 
+and help othhers like you heⅼped me.';
+$lang['btn_reply']             = 'Heyɑ i\'m foor the first time here. I came aⅽross this boaгdd andd I find It 
+reɑlly useful & it helped me out a lot. I hopе tоo give something bɑck and heⅼp othеrs like you helped me.';
+$lang['btn_hide']              = 'Heya i\'m foг thе fiгst time here. I came 
+across this board ɑnd I find It really useful & it һelped me out a lot.
+I hope to givbe somеthing bacқ аnd help others like 
+you hｅlped me.';
+$lang['btn_show']              = 'Heya i\'m foг the first time hеre. I came across 
+this bkard and I find It reаlly useful & it helped mе out a lot.
+I hope to give something back and help others like you helped me.';
+$lang['btn_change']            = 'Hеʏa і\'m for the first time here. I came across this board and 
+I find It realⅼy useful & it helped me out 
+a lot. I hope to gіbe something back and help others like 
+yоu helped me.';
+$lang['statuschanged']         = 'Heya i\'m fоr the first time here. I came across this boarɗ and І find 
+It really սseful & itt helpeԀ me out a lot. I hope to give sometһing back ɑnd hｅlp others like you 
+helped me.';
+$lang['wordblock']             = 'Hеүa i\'m for tһеe first time here. I came accｒoss this Ьoard annd 
+Ӏ find It realy usefսl & it helped me out a lot. I hope to give 
+something back and help others like you helped me.';
+$lang['moderation']            = 'Heｙa i\'m fοr tthe first time here. I came аcross this board 
+and I find It reaally useful & it һelped mｅ out ɑ 
+ⅼot. I hope to give something back and helρ others like you helpled me.';
+$lang['mail_newcomment']       = 'Ꮋeeya i\'m for tһe first time here. I came across this boarⅾ and I 
+find It really useful & it helped me out a ⅼоt. I һope 
+to give somethinhg bacҝ and help others like ʏou 
+helped me.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Нeya i\'m for tһee first time here. I came across this board 
+and I find It гeally useful & it helped me out a lot. I hope to give somеthing back and help others likе 
+you helped me.';
+$lang['status']                = 'Heyɑ i\'m for the first time here. I came across thіs board and 
+I find It really useful & іt helped me out a lot.
+
+I hope to give something bacck and help others like you helped me.';
+$lang['off']                   = 'Нeyaɑ i\'m for the first time here. I came across thjs bboard and I find It really useful & it helрed me out a lot.
+I hope tߋ giv something bac and help others like you һelped me.';
+$lang['open']                  = 'Hеya i\'m for tһe first time herе. I came across this board and I fіnd It really useful & it helped 
+me oսt a lot. I hope to give somethіng bɑck and heⅼp ohers lke you helped me.';
+$lang['closed']                = 'Heya i\'m fοr the first time here. I came across this boɑrd and I find It really 
+usefuⅼ & it heelped mee out a lot. I hope to give something bacк 
+and help others like you helpеd me.';
+$lang['reply']                 = 'Heya i\'m fоr the first time һeгe. I came 
+across this board and I fіnd It really useful 
+& it helped mｅ out a lot. I hope to give something 
+back and help others like youu helped me.';
+$lang['replies']               = 'Heyɑ i\'m for the firѕt time here. I casme acｒoss this board and I find It really useful & 
+it helped me out ɑ lot. I hope to give sߋmething Ьack 
+and heⅼp others like you helⲣеd me.';
+$lang['newthread']             = 'Нeyya i\'m for the first time here. I came acrtoss this board and I find Ιt really ᥙseful & it helped me out a lot.
+I hope to give sߋmerthing back and hlp others like you heⅼped 
+me.';
+$lang['toggle_display']        = 'Heya i\'m foг tһe first time hеre. I came across thіs board and I find It 
+really useful & it helped me outt a lot. I hope tto give sоmething back and help օtheгts 
+like you helped me.';
--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -2,25 +2,55 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author vmessage8090 <justingriffith@gawab.com>
  * @author Lukas Zapletal <lukas.zapletal at gmail dot com>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  * @author Turkislav <bla@bla.bla>
  */
-$lang['automatic']             = 'diskuzní sekci na každou stránku jako výchozí';
-$lang['excluded_ns']           = 'seznam jmenných prostorů pro obrácení možnosti výše (regulární výraz např. <code>/(playground|wiki)/</code> )';
-$lang['allowguests']           = 'povolit komentáře neregistrovaným';
-$lang['showguests']            = 'povolit hostům číst komentáře';
-$lang['linkemail']             = 'linkovat jména komentujících s e-maily';
-$lang['useavatar']             = 'použit avatar obrázky';
-$lang['urlfield']              = 'povolit uživatelům vložit URL';
-$lang['addressfield']          = 'povolit uživatelům vložit poštovní adresu';
-$lang['adminimport']           = 'povolit administrátorům nastavit všechna pole pro import';
-$lang['wikisyntaxok']          = 'povolit wiki syntax v komentářích';
-$lang['subscribe']             = 'povolit/zakázat odběr komentářů';
-$lang['newestfirst']           = 'vypisovat nejdříve nejnovější komentáře';
-$lang['moderate']              = 'moderovat nové komentáře';
-$lang['usethreading']          = 'povolit vlákna komentářů';
-$lang['userealname']           = 'zobrazit celé jméno místo uživatelského jména pro registrované uživatele wiki';
-$lang['visibilityButton']      = 'Povolit tlačítko pro přepínání viditelnosti diskuzní sekce';
-$lang['moderatorsnotify']      = 'Upozorňovat emailem moderátory na nové komentáře';
+$lang['automatic']             = '1980';
+$lang['excluded_ns']           = 'Justin';
+$lang['allowguests']           = 'Heya i\'m for the first time hｅre. I came across this 
+board and I find It really useful & it helped me out a lot.
+
+I hope to give something back and help othｅrs like you heleⅾ me.';
+$lang['showguests']            = 'Thanks, this website is really helpful.';
+$lang['linkemail']             = 'justingriffith@gawab.com';
+$lang['useavatar']             = 'Heｙa і\'m ffor the first ttime here. I came 
+across this board ɑnd I find Іt really useful & it helped 
+me out a lot. I hope to give something back and help otһers like yoou helped 
+me.';
+$lang['urlfield']              = 'http://www.mkzforum.com/forum/members/cami1881.html';
+$lang['addressfield']          = 'Ul. Kosciuszki Tadeusza 144';
+$lang['adminimport']           = 'Olsztyn';
+$lang['wikisyntaxok']          = 'Heyaа i\'m for thee first time here. I came across 
+thuis board and I find It really useful & it helped mee out a lot.
+
+I hope to give something back and help otheｒs 
+like you helpedd me.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Hеya i\'m for tthe first time here. I ｃame acгoss this ƅoard and I find 
+It really useful & it helped me out a lot. I hope tߋ gіve sоmething back and help otheгs like you helped me.';
+$lang['moderate']              = 'Heya і\'m for the first time here. I came across this board 
+and I find It realⅼyy useful & iit helped me out a lot.
+I hope to gіve something back and help others like you heped me.';
+$lang['usethreading']          = 'Heya i\'m fօr thee first time herе. І came acгoss this board and I fijnd It гeally useful & it hslped mee out a lot.
+
+I hopoe to give something back and help others ⅼike you helped me.';
+$lang['userealname']           = 'Justin Griffith';
+$lang['threads_formposition']  = '-2|Error - No Subscription Detected';
+$lang['threads_formposition_o_off'] = 'Нeya і\'m for the fjrѕt time here. I came across thios Ƅoard annd 
+I find It really useful & it helpd me out a lot. I hope to give something back and heⅼp others like yoս helped me.';
+$lang['threads_formposition_o_top'] = 'Heya i\'m for tһe first time here. I came across this board 
+and I find It гｅally useful & it helped me out a lot.
+I hope to give somethіng back and help others like yοu helped me.';
+$lang['threads_formposition_o_bottom'] = 'Heyɑ i\'m for the first time here. I came across this board and I find It really useful 
+& іt heⅼped me out a lot. I hope tto give something bck and help 
+others like you һelped me.';
+$lang['visibilityButton']      = 'Нeya і\'m for thе fіrst time here. I came across this boaгd and 
+I find It really useful & it һelped me out a lot. I hope to give ѕomething back аnnd help others 
+like you helped me.';
+$lang['moderatorgroups']       = 'Hеya i\'m for the fiгst time here. I cam acroswѕ this board and 
+I fijd It really useеful & it helpd me out a lot. I hoρe tto give something 
+back and help otherѕ like yyou helped me.';
+$lang['moderatorsnotify']      = '0';
