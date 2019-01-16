07fbd021bf8b9e8ee7272dd202800353e1e57244
Tibor Repček <tiborepcek@gmail.com>
2019-01-16 14:44:42 +0100

    Update settings.php

--- lang/sk/settings.php
+++ lang/sk/settings.php
@@ -4,11 +4,35 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Pirhala Marek <mpirhala@genesispo.sk>
+ * @author     Tibor Repček <tiborepcek@gmail.com>
  */
  
 // for the configuration manager
+$lang['automatic']    = 'diskusia sa bude zobrazovať na všetkých stránkach';
+$lang['excluded_ns']  = 'zoznam menných priestorov, na ktorom diskusia nebude zobrazená (regulárny výraz ako <code>/(playground|wiki)/</code> )';
 $lang['allowguests'] = 'povoliť komentáre neregistrovaným';
+$lang['showguests']   = 'povoliť neregistrovaným čítať komentáre';
 $lang['linkemail']   = 'linkovať mená komentujúcich s e-mailami';
 $lang['useavatar']   = 'použiť obrázky gavatar';
+$lang['urlfield']     = 'povoliť používateľom zadať webovú stránku';
+$lang['addressfield'] = 'povoliť používateľom zadať poštové smerovacie číslo';
+$lang['adminimport']  = 'povoliť administrátorom vybrať všetky polia na importovanie';
+$lang['wikisyntaxok'] = 'povoliť wiki syntax v komentároch';
+$lang['subscribe']    = 'povoliť/zakázať odoberať komentáre';
+$lang['newestfirst']  = 'zobraziť najnovšie komentáre hore';
+$lang['moderate']     = 'moderovať nové komentáre';
+
+$lang['usethreading'] = 'povoliť vetvenie komentárov';
+$lang['userealname']  = 'zobrazovať reálne mená a nie používateľské mená registrovaných používateľov';
+
+$lang['threads_formposition']          = 'umiestnenie formulára diskusie pomocou syntaxe <code>{{threads> }}</code>';
+$lang['threads_formposition_o_off']    = 'vypnuté';
+$lang['threads_formposition_o_top']    = 'hore';
+$lang['threads_formposition_o_bottom'] = 'dole';
+
+$lang['visibilityButton'] = 'Povoliť tlačidlo na skrytie/zobrazenie diskusie';
+
+$lang['moderatorgroups']  = 'Čiarkou oddelený zoznam skupín moderátora (ako v Správe zoznamu prístupových práv)';
+$lang['moderatorsnotify']  = 'Poslať moderátorom upozornenie na nový komentár e-mailom';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
