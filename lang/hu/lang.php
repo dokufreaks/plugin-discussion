91c43437e9331484cde89d076aa585670e7de237
carpet <philippstrahan@reallyfast.biz>
2020-04-07 02:00:37 +0200

    translation update

--- lang/hu/lang.php
+++ lang/hu/lang.php
@@ -2,38 +2,135 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author carpet <philippstrahan@reallyfast.biz>
  * @author Mátyás Jani <jzombi@gmail.com>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['menu']                  = 'Hozzászólások moderálása';
-$lang['discussion']            = 'Hozzászólások';
-$lang['comment']               = 'Komment';
-$lang['comments']              = 'Komment';
-$lang['nocomments']            = 'Komment';
-$lang['url']                   = 'Weboldal';
+$lang['encoding']              = 'I\'ve been browsing online more than 3 hours today, yet I never found 
+any interesting article like yours. It is pretty worth enough for me.
+
+In my view, if all webmasters and bloggers made good content as you did, the web will be much more useful 
+than ever before.';
+$lang['direction']             = 'I\'ve been browsing online more than 3 hours today,
+yet I never found any interesting article like yours. It is pretty worth enough for me.
+
+In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
+$lang['menu']                  = 'I\'ve been browsing online more than 3 hours today, yet I never found any 
+interesting article like yours. It is pretty worth enough for me.
+
+In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
+$lang['discussion']            = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as 
+you did, the web will be much more useful 
+than ever before.';
+$lang['comment']               = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my 
+view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
+$lang['comments']              = 'I\'ve been browsing online more than 3 hours today, 
+yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much more useful than ever before.';
+$lang['nocomments']            = 'I\'ve been browsing online more than 3 hours today,
+yet I never found any interesting article like yours. It is 
+pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did,
+the web will be much more useful than ever before.';
+$lang['threads']               = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like 
+yours. It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much more useful than ever before.';
+$lang['url']                   = 'http://elcampocarpetoneelcampo.com,bernardoutcarpets.co.uk,carpets-curtains.co.uk,axminster100.co.uk,rugsforallrooms.com,carpet-online.co.uk,cc-furnishing.co.uk,alchamico.com,belvederecarpets.co.uk,bobscustomcarpets.com,acsdorsettcarpet.com,judsoncarpets.co.uk,dowcarpetlatex.com,carpet-cleaning-cardiff-uk.co.ukcholleton.co.uk,fairfaxflooralexandriacarpet.com,marshallcarpets.co.uk,leighcarpettiles.co.uk,renewalcarpettiles.com,noahsarkcarpetskilims.com,stevebirchcleaningbirmingham.co.uk,tesseracarpettiles.com,carpettilefloor.com,commercialcarpettilescheap.com,www.fairfaxflooralexandriacarpet.com,ara-carpet.com';
 $lang['address']               = 'Cím';
-$lang['date']                  = 'Dátum';
-$lang['entercomment']          = 'Hozzászólás írása';
-$lang['wikisyntax']            = 'Wiki szintaxis engedélyezett';
-$lang['older']                 = 'Korábbi hozzászólások';
-$lang['newer']                 = 'Újabb hozzászólások';
-$lang['btn_reply']             = 'Válasz';
-$lang['btn_hide']              = 'Elrejtés';
-$lang['btn_show']              = 'Megjelenítés';
-$lang['btn_change']            = 'Változtat';
-$lang['statuschanged']         = 'Hozzászólás státusza megváltozott';
-$lang['wordblock']             = 'A hozzászólásod spamnek lett megjelölve.';
-$lang['moderation']            = 'A hozzászólásod moderálásra vár.';
-$lang['mail_newcomment']       = 'Új komment';
-$lang['subscribe']             = 'Feliratkozás a hozzászólásokra';
-$lang['noguests']              = 'Hozzászóláshoz be kell jelentkezni.';
-$lang['status']                = 'Státusz';
-$lang['off']                   = 'kikapcsolt';
-$lang['open']                  = 'nyitott';
-$lang['closed']                = 'lezárt';
-$lang['reply']                 = 'Válasz';
-$lang['replies']               = 'Válaszok';
-$lang['newthread']             = 'Új hozzászólások oldal';
-$lang['toggle_display']        = 'Elrejtés/Megjelenítés';
+$lang['date']                  = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did, the web will be 
+much more useful than ever before.';
+$lang['entercomment']          = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did,
+the web will be much more useful than ever before.';
+$lang['wikisyntax']            = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made 
+good content as you did, the web will be much more useful than ever before.';
+$lang['older']                 = 'I\'ve been browsing online more than 3 hours today, 
+yet I never found any interesting article like yours. It 
+is pretty worth enough for me. In my view, if all webmasters and bloggers made good content 
+as you did, the web will be much more useful than ever before.';
+$lang['newer']                 = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting 
+article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, the web 
+will be much more useful than ever before.';
+$lang['btn_reply']             = 'I\'ve been browsing online more than 3 hours 
+today, yet I never found any interesting article like yours.
+
+It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much 
+more useful than ever before.';
+$lang['btn_hide']              = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, the web will be 
+much more useful than ever before.';
+$lang['btn_show']              = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever 
+before.';
+$lang['btn_change']            = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
+$lang['statuschanged']         = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much more useful 
+than ever before.';
+$lang['wordblock']             = 'I\'ve been browsing online more than 3 hours today, yet I never found 
+any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, the web will be much more useful than ever before.';
+$lang['moderation']            = 'I\'ve been browsing online more than 3 hours today, yet I never found any 
+interesting article like yours. It is pretty worth enough for me.
+
+In my view, if all webmasters and bloggers made good content as you did, the 
+web will be much more useful than ever before.';
+$lang['mail_newcomment']       = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made 
+good content as you did, the web will be much more 
+useful than ever before.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'I\'ve been browsing online more than 3 hours today,
+yet I never found any interesting article like yours.
+
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did, the web will be much more useful 
+than ever before.';
+$lang['status']                = 'I\'ve been browsing online more than 3 hours today, 
+yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view,
+if all webmasters and bloggers made good content as you did, the web will be much more useful than ever before.';
+$lang['off']                   = 'I\'ve been browsing online more than 3 hours today, yet I never found 
+any interesting article like yours. It is pretty worth enough for 
+me. In my view, if all webmasters and bloggers made good content as you did, the web will be much 
+more useful than ever before.';
+$lang['open']                  = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made 
+good content as you did, the web will be much more 
+useful than ever before.';
+$lang['closed']                = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like 
+yours. It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did, the web will be much more 
+useful than ever before.';
+$lang['reply']                 = 'I\'ve been browsing online more than 3 hours today, yet I 
+never found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, the web will be much 
+more useful than ever before.';
+$lang['replies']               = 'I\'ve been browsing online more than 3 hours today, yet 
+I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much more useful 
+than ever before.';
+$lang['newthread']             = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
+$lang['toggle_display']        = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good 
+content as you did, the web will be much more useful than ever before.';
--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -2,27 +2,64 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author carpet <philippstrahan@reallyfast.biz>
  * @author Norbert Csík <norbert.csik@gmail.com>
  * @author Mátyás Jani <jzombi@gmail.com>
  */
-$lang['automatic']             = 'hozzászólások bekapcsolása alapértelmezettként minden oldalon';
-$lang['allowguests']           = 'nem regisztrált felhasználók kommentelhetnek';
-$lang['showguests']            = 'vendégek olvashatják a kommenteket';
-$lang['linkemail']             = 'legyen hivatkozás a hozzászóló e-mail címével a néven';
-$lang['useavatar']             = 'használjon avatar képeket a hozzászólásoknál';
-$lang['urlfield']              = 'felhasználók megadhatnak egy weboldalt';
-$lang['addressfield']          = 'felhasználók megadhatják a címüket';
-$lang['adminimport']           = 'adminok hozzászóláskor minden mezőt külön megadhatnak';
-$lang['wikisyntaxok']          = 'wiki szintaxis engedélyezése kommentekben';
-$lang['subscribe']             = 'kommentekre feliratkozás engedélyezése';
-$lang['newestfirst']           = 'újabb hozzászólásokat mutassa fölül';
-$lang['moderate']              = 'új kommentek moderálása';
-$lang['usethreading']          = 'kommentek szálakba rendezése';
-$lang['userealname']           = 'valódi név megjelenítése felhasználói név helyett regisztrált felhasználóknál';
-$lang['threads_formposition']  = 'új szál form pozíciója';
-$lang['threads_formposition_o_off'] = 'kikapcsolva';
-$lang['threads_formposition_o_top'] = 'fölül';
-$lang['threads_formposition_o_bottom'] = 'alul';
-$lang['visibilityButton']      = 'hozzászólások elrejtése/megjelenítése gomb bekapcsolása';
-$lang['moderatorgroups']       = 'Moderátor csoportok vesszővel elválasztott listája (megegyezik a DokuWiki-Manager hozzáférési szinttel)';
+$lang['automatic']             = '1981';
+$lang['excluded_ns']           = 'Philipp';
+$lang['allowguests']           = 'I\'ve been browsing online more than 3 hours today, yet I never found any 
+interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, 
+the web will be much more useful than ever before.';
+$lang['showguests']            = 'Surprisingly individual friendly website. Immense information readily available on few clicks.';
+$lang['linkemail']             = 'philippstrahan@reallyfast.biz';
+$lang['useavatar']             = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting 
+article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made 
+good content as you did, the web will be much more useful than ever before.';
+$lang['urlfield']              = 'http://elcampocarpetoneelcampo.com,bernardoutcarpets.co.uk,carpets-curtains.co.uk,axminster100.co.uk,rugsforallrooms.com,carpet-online.co.uk,cc-furnishing.co.uk,alchamico.com,belvederecarpets.co.uk,bobscustomcarpets.com,acsdorsettcarpet.com,judsoncarpets.co.uk,dowcarpetlatex.com,carpet-cleaning-cardiff-uk.co.ukcholleton.co.uk,fairfaxflooralexandriacarpet.com,marshallcarpets.co.uk,leighcarpettiles.co.uk,renewalcarpettiles.com,noahsarkcarpetskilims.com,stevebirchcleaningbirmingham.co.uk,tesseracarpettiles.com,carpettilefloor.com,commercialcarpettilescheap.com,www.fairfaxflooralexandriacarpet.com,ara-carpet.com';
+$lang['addressfield']          = 'Spiegelsberg 67';
+$lang['adminimport']           = 'Altweidling';
+$lang['wikisyntaxok']          = 'I\'ve been browsing online more than 3 hours today, 
+yet I never found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did, the web will be much more 
+useful than ever before.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting 
+article like yours. It is pretty worth enough for me. In my view, if all 
+webmasters and bloggers made good content as you did, the web will be much more 
+useful than ever before.';
+$lang['moderate']              = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like 
+yours. It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as 
+you did, the web will be much more useful than ever before.';
+$lang['usethreading']          = 'I\'ve been browsing online more than 3 hours today, yet 
+I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and bloggers 
+made good content as you did, the web will be much more useful than ever before.';
+$lang['userealname']           = 'Philipp Strahan';
+$lang['threads_formposition']  = '0';
+$lang['threads_formposition_o_off'] = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+It is pretty worth enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much 
+more useful than ever before.';
+$lang['threads_formposition_o_top'] = 'I\'ve been browsing online more than 3 hours today, yet I never 
+found any interesting article like yours. It is pretty worth enough for me.
+In my view, if all webmasters and bloggers made good content as you did,
+the web will be much more useful than ever before.';
+$lang['threads_formposition_o_bottom'] = 'I\'ve been browsing online more than 3 hours today, yet I never found any interesting article like yours.
+
+It is pretty worth enough for me. In my view, if all webmasters and bloggers made good content as you did, the 
+web will be much more useful than ever before.';
+$lang['visibilityButton']      = 'I\'ve been browsing online more than 3 hours today, yet 
+I never found any interesting article like yours. It is pretty worth 
+enough for me. In my view, if all webmasters and 
+bloggers made good content as you did, the web will be much more useful than ever 
+before.';
+$lang['moderatorgroups']       = 'I\'ve been browsing online more than 3 hours today, yet 
+I never found any interesting article like yours. It 
+is pretty worth enough for me. In my view, 
+if all webmasters and bloggers made good content as you did, the web will 
+be much more useful than ever before.';
+$lang['moderatorsnotify']      = '0';
