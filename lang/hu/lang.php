69afc09ad4bf7a19fa894a89baa8a2b5714a95aa
strategi-hom <florbodnar@gawab.com>
2020-04-22 10:20:12 +0200

    translation update

--- lang/hu/lang.php
+++ lang/hu/lang.php
@@ -2,38 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author strategi-hom <florbodnar@gawab.com>
  * @author Mátyás Jani <jzombi@gmail.com>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['menu']                  = 'Hozzászólások moderálása';
-$lang['discussion']            = 'Hozzászólások';
-$lang['comment']               = 'Komment';
-$lang['comments']              = 'Komment';
-$lang['nocomments']            = 'Komment';
-$lang['url']                   = 'Weboldal';
+$lang['url']                   = 'http://v-lada.ru/user/EileenTarpley91/';
 $lang['address']               = 'Cím';
-$lang['date']                  = 'Dátum';
-$lang['entercomment']          = 'Hozzászólás írása';
-$lang['wikisyntax']            = 'Wiki szintaxis engedélyezett';
-$lang['older']                 = 'Korábbi hozzászólások';
-$lang['newer']                 = 'Újabb hozzászólások';
-$lang['btn_reply']             = 'Válasz';
-$lang['btn_hide']              = 'Elrejtés';
-$lang['btn_show']              = 'Megjelenítés';
-$lang['btn_change']            = 'Változtat';
-$lang['statuschanged']         = 'Hozzászólás státusza megváltozott';
-$lang['wordblock']             = 'A hozzászólásod spamnek lett megjelölve.';
-$lang['moderation']            = 'A hozzászólásod moderálásra vár.';
-$lang['mail_newcomment']       = 'Új komment';
-$lang['subscribe']             = 'Feliratkozás a hozzászólásokra';
-$lang['noguests']              = 'Hozzászóláshoz be kell jelentkezni.';
-$lang['status']                = 'Státusz';
-$lang['off']                   = 'kikapcsolt';
-$lang['open']                  = 'nyitott';
-$lang['closed']                = 'lezárt';
-$lang['reply']                 = 'Válasz';
-$lang['replies']               = 'Válaszok';
-$lang['newthread']             = 'Új hozzászólások oldal';
-$lang['toggle_display']        = 'Elrejtés/Megjelenítés';
+$lang['subscribe']             = '0';
--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -2,27 +2,19 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author strategi-hom <florbodnar@gawab.com>
  * @author Norbert Csík <norbert.csik@gmail.com>
  * @author Mátyás Jani <jzombi@gmail.com>
  */
-$lang['automatic']             = 'hozzászólások bekapcsolása alapértelmezettként minden oldalon';
-$lang['allowguests']           = 'nem regisztrált felhasználók kommentelhetnek';
-$lang['showguests']            = 'vendégek olvashatják a kommenteket';
-$lang['linkemail']             = 'legyen hivatkozás a hozzászóló e-mail címével a néven';
-$lang['useavatar']             = 'használjon avatar képeket a hozzászólásoknál';
-$lang['urlfield']              = 'felhasználók megadhatnak egy weboldalt';
-$lang['addressfield']          = 'felhasználók megadhatják a címüket';
-$lang['adminimport']           = 'adminok hozzászóláskor minden mezőt külön megadhatnak';
-$lang['wikisyntaxok']          = 'wiki szintaxis engedélyezése kommentekben';
-$lang['subscribe']             = 'kommentekre feliratkozás engedélyezése';
-$lang['newestfirst']           = 'újabb hozzászólásokat mutassa fölül';
-$lang['moderate']              = 'új kommentek moderálása';
-$lang['usethreading']          = 'kommentek szálakba rendezése';
-$lang['userealname']           = 'valódi név megjelenítése felhasználói név helyett regisztrált felhasználóknál';
-$lang['threads_formposition']  = 'új szál form pozíciója';
-$lang['threads_formposition_o_off'] = 'kikapcsolva';
-$lang['threads_formposition_o_top'] = 'fölül';
-$lang['threads_formposition_o_bottom'] = 'alul';
-$lang['visibilityButton']      = 'hozzászólások elrejtése/megjelenítése gomb bekapcsolása';
-$lang['moderatorgroups']       = 'Moderátor csoportok vesszővel elválasztott listája (megegyezik a DokuWiki-Manager hozzáférési szinttel)';
+$lang['automatic']             = '1990';
+$lang['excluded_ns']           = 'Flor';
+$lang['showguests']            = 'A Saint-Valery-sur-Somme, les résidents peuvent bénéficier du salon de coiffure ou du déplacement de la coiffeuse dans le service, prestation prévue dans le séjour. Une coiffeuse salariée de l’EHPAD se tient à votre disposition sur rendez-vous. Qui ne dit rien consent ou alors est-ce une forme de manière de me faire comprendre qu\'en fait les familles et le résident ne sont pas plus informés qu\'eux ? La fidélisation sur un poste et un endroit n\'est plus à la mode pour la jeune génération: « Leur culture de l\'intuito-connectif leur donne très vite le sentiment d\'avoir fait le tour et de n\'avoir plus rien à apprendre103 ». Il motive son équipe, afin d\'impliquer le plus de professionnels possibles. Son amélioration est demandée par tous, mais encore faut-il l\'avoir construit et employé par tous. Il est plus simple de se dire qu\'en absence de temps, ce sont les anciens usages qui sont les plus adaptés.';
+$lang['linkemail']             = 'florbodnar@gawab.com';
+$lang['urlfield']              = 'http://v-lada.ru/user/EileenTarpley91/';
+$lang['addressfield']          = 'Rue Du Monument 317';
+$lang['adminimport']           = 'Angreau';
+$lang['subscribe']             = '0';
+$lang['userealname']           = 'Flor Bodnar';
+$lang['threads_formposition']  = '.';
+$lang['moderatorsnotify']      = '0';
