ee793fe38d2b8e073e3fc35f163dd268179f719f
Gerrit Uitslag <klapinklapin@gmail.com>
2022-09-19 00:11:39 +0200

    js: reformatting

--- script.js
+++ script.js
@@ -5,41 +5,41 @@
 /**
  * Check if a field is blank
  */
-function isBlank(s){
-  if ((s === null) || (s.length === 0)){
-    return true;
-  }
-
-  for (var i = 0; i < s.length; i++){
-    var c = s.charAt(i);
-	  if ((c !== ' ') && (c !== '\n') && (c !== '\t')){
-	    return false;
+function isBlank(s) {
+    if ((s === null) || (s.length === 0)) {
+        return true;
     }
-  }
-  return true;
+
+    for (var i = 0; i < s.length; i++) {
+        var c = s.charAt(i);
+        if ((c !== ' ') && (c !== '\n') && (c !== '\t')) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /**
  * Validate an input field
  */
-function validate(form){
-    if(!form) return;
+function validate(form) {
+    if (!form) return;
 
-    if (isBlank(form.name.value)){
+    if (isBlank(form.name.value)) {
         form.name.focus();
         form.name.style.backgroundColor = '#fcc';
         return false;
     } else {
         form.name.style.backgroundColor = '#fff';
     }
-    if (isBlank(form.mail.value) || form.mail.value.indexOf("@") === -1){
+    if (isBlank(form.mail.value) || form.mail.value.indexOf("@") === -1) {
         form.mail.focus();
         form.mail.style.backgroundColor = '#fcc';
         return false;
     } else {
         form.mail.style.backgroundColor = '#fff';
     }
-    if (isBlank(form.text.value)){
+    if (isBlank(form.text.value)) {
         form.text.focus();
         form.text.style.borderColor = '#fcc';
         return false;
@@ -60,12 +60,12 @@ function discussion_ajax_preview() {
         $preview.hide();
         return;
     }
-    $preview.html('<img src="'+DOKU_BASE+'lib/images/throbber.gif" />');
+    $preview.html('<img src="' + DOKU_BASE + 'lib/images/throbber.gif" />');
     $preview.show();
 
     jQuery.post(DOKU_BASE + 'lib/exe/ajax.php',
         {
-            'call': 'discussion_preview',
+            'call':    'discussion_preview',
             'comment': comment
         },
         function (data) {
@@ -80,9 +80,9 @@ function discussion_ajax_preview() {
         }, 'html');
 }
 
-jQuery(function() {
+jQuery(function () {
     // init toolbar
-    if(typeof window.initToolbar == 'function') {
+    if (typeof window.initToolbar == 'function') {
         initToolbar("discussion__comment_toolbar", "discussion__comment_text", toolbar);
     }
 
@@ -90,10 +90,12 @@ jQuery(function() {
     jQuery('#discussion__btn_preview').on('click', discussion_ajax_preview);
 
     // init field check
-    jQuery('#discussion__comment_form').on('submit',function() { return validate(this); });
+    jQuery('#discussion__comment_form').on('submit', function () {
+        return validate(this);
+    });
 
     // toggle section visibility
-    jQuery('#discussion__btn_toggle_visibility').on('click', function() {
+    jQuery('#discussion__btn_toggle_visibility').on('click', function () {
         jQuery('#comment_wrapper').toggle();
     });
 });
