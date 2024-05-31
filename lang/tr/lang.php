2f1c5eb8bcede0e587a3b899f0e936d644f10874
diyarbakir Eskort bayan <arronbiaggini@live.fr>
2024-05-31 05:25:07 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,48 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author diyarbakir Eskort bayan <arronbiaggini@live.fr>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
-
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
-
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
-
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['encoding']              = 'Appreciate the recommendation. Let me try it out.';
+$lang['direction']             = 'Appreciate the recommendation. Let me try it out.';
+$lang['menu']                  = 'Appreciate the recommendation. Let me try it out.';
+$lang['discussion']            = 'Appreciate the recommendation. Let me try it out.';
+$lang['comment']               = 'Appreciate the recommendation. Let me try it out.';
+$lang['comments']              = 'Appreciate thhe recommendation. Let mme try 
+it out.';
+$lang['nocomments']            = 'Appreciate the recommendation. Let me try it out.';
+$lang['threads']               = 'Appreciate the recommendation. Lett me try it out.';
+$lang['url']                   = 'https://www.rexart.com/cgi-rexart/al/affiliates.cgi?aid=872&redirect=https://justpaste.it/diyarbakireskortrehberi';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'Appreciate the recommendation. Let me try 
+it out.';
+$lang['entercomment']          = 'Appreciate the recommendation. Let me try it out.';
+$lang['wikisyntax']            = 'Apprecoate the recommendation. Let mme try it out.';
+$lang['older']                 = 'Appreciate the recommendation. Let me try it out.';
+$lang['newer']                 = 'Appreciate the recommendation. Let me try it out.';
+$lang['btn_reply']             = 'Appreciate the recommendation. Let me try it out.';
+$lang['btn_hide']              = 'Appreciate the recommendation. Let me try it out.';
+$lang['btn_show']              = 'Appreciate the recommendation. Let me try it out.';
+$lang['btn_change']            = 'Appreciate the recommendation. Let me try it out.';
+$lang['statuschanged']         = 'Appreciate the recommendation. Let me try it out.';
+$lang['wordblock']             = 'Apprreciate the recommendation. Let me try it out.';
+$lang['moderation']            = 'Appreciate the recommendation. Let me try itt out.';
+$lang['mail_newcomment']       = 'Appreciate the recommendation. Let me try it out.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Appreciate the recommendation. Let me try it out.';
+$lang['status']                = 'Appreciate the recommendation. Let me try it out.';
+$lang['off']                   = 'Appreciate the recommendation. Let me try it out.';
+$lang['open']                  = 'Appreciate the recommendation. Let me trry it out.';
+$lang['closed']                = 'Appreciate the recommendation. Let me try itt out.';
+$lang['reply']                 = 'Appreciate the recommendation. Let me try it out.';
+$lang['replies']               = 'Appreciate tthe recommendation. Let me try it out.';
+$lang['newthread']             = 'Appreciate the recommendation. Leet me try it out.';
+$lang['toggle_display']        = 'Appreciate the recommendation. Let me try it out.';
+$lang['js']['confirmdelete']   = 'Appreciate the recommendation. Let me try it out.';
---
+++ lang/tr/settings.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author diyarbakir Eskort bayan <arronbiaggini@live.fr>
+ */
+$lang['automatic']             = '1979';
+$lang['excluded_ns']           = 'Arron';
+$lang['allowguests']           = 'Appreciate the recommendation. Let me try it out.';
+$lang['showguests']            = 'I conceive this internet site has got some real good info for everyone :D.';
+$lang['linkemail']             = 'arronbiaggini@live.fr';
+$lang['useavatar']             = 'Appreciate the recommendation. Lett me try it out.';
+$lang['urlfield']              = 'https://www.rexart.com/cgi-rexart/al/affiliates.cgi?aid=872&redirect=https://justpaste.it/diyarbakireskortrehberi';
+$lang['addressfield']          = '4 Ponteland Rd';
+$lang['adminimport']           = 'Hubbert\'s Bridge';
+$lang['wikisyntaxok']          = 'Appreciate the recommendation. Let me try it 
+out.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Appreciate the recommendation. Let me try it out.';
+$lang['moderate']              = 'Appreciate thhe recommendation. Let me try it out.';
+$lang['usethreading']          = 'Appreciate the recommendation. Let me try it out.';
+$lang['userealname']           = 'Arron Biaggini';
+$lang['threads_formposition']  = 'position of the new thread form shown by the {{threads> }} syntax';
+$lang['threads_formposition_o_off'] = 'Appreciate the recommendation. Let me try it out.';
+$lang['threads_formposition_o_top'] = 'Appreciate the recommendation. Let me try it out.';
+$lang['threads_formposition_o_bottom'] = 'Appreciate the recommendation. Let me try it out.';
+$lang['visibilityButton']      = 'Appreciate the recommendation. Lett mme try it out.';
+$lang['moderatorgroups']       = 'Appreciate the recommendation. Let me try it out.';
+$lang['moderatorsnotify']      = '0';
