296f60ea57fbebdba966ba4711d3dc88b17308f9
diyarbakir escort <aurelia.layton@sfr.fr>
2024-06-06 11:40:07 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,130 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author diyarbakir escort <aurelia.layton@sfr.fr>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'I think this iss among the most important informarion for 
+me. And i\'m glad reading your article. But want to remark 
+on few general things, The website style iss ideal, the 
+articles is really great : D. Good job, cheers';
+$lang['direction']             = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want too remark on few general things, The website style is ideal, thee articles is really great :
+D. Good job, cheers';
+$lang['menu']                  = 'I think this is among the most important information forr me.
+And i\'m glad reading your article. But want to 
+remark on few general things, The website style is ideal, 
+the articles is really great : D. Good job, cheers';
+$lang['discussion']            = 'I think this is among the most important information for 
+me. And i\'m glad reading your article. But want to remark on few general 
+things, The website style is ideal, the articles is really great : D.
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
+Good job, cheers';
+$lang['comment']               = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark onn few general things, The website style is ideal, the articles is really great : D.
+Good job, cheers';
+$lang['comments']              = 'I think this iss among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The 
+website stype is ideal, the articles is really great : D.
+Good job, cheers';
+$lang['nocomments']            = 'I think this is among the most important information for me.
+And i\'m glad reading your article. Buut want to remarkk on few 
+geberal things, Thhe website style is ideal, the articles is really great :
+D. Good job, cheers';
+$lang['threads']               = 'I think this is among the most important informatgion for me.
 
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
-
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+And i\'m glad reading yoiur article. But want to remark on few general 
+things, The website style is ideal, the articles is really great : D.
+Good job, cheers';
+$lang['url']                   = 'https://Thesocialcircles.com/story2812543/9-best-quick-fat-loss-tips-for-teenage-girls';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'I hink this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The website style is ideal, the articles is really great :
+D. Good job, cheers';
+$lang['entercomment']          = 'I think this is among the most important information for me.
+Andd i\'m glad reading your article. But want to remark on few general things, The website style is ideal, the 
+articles is really great : D. Good job, cheers';
+$lang['wikisyntax']            = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The website style 
+is ideal, the articles iss really great : D.
+Good job, cheers';
+$lang['older']                 = 'I think this iss amoong the most important information for me.
+And i\'m glad reading your article. But want tto remark on few general things, The 
+webbsite style is ideal,the articles is really great : D.
+Good job, cheers';
+$lang['newer']                 = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark 
+on few general things, The website style is 
+ideal, the articles is really gret : D. Good job, cheers';
+$lang['btn_reply']             = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, Thee website tyle is ideal, the articls is really great : D.
+Good job, cheers';
+$lang['btn_hide']              = 'I think this is aong the most important information for me.
+And i\'m glad reading your article. But want to remark 
+on few general things, The website style is ideal, the articles 
+is really great : D. Good job, cheers';
+$lang['btn_show']              = 'I think this is among the most important informastion for me.
+And i\'m glad reading your article. But want to remsrk 
+on few gensral things, The website style is ideal, the articles is really greeat 
+: D. Good job, cheers';
+$lang['btn_change']            = 'I think this is among the most important information for me.
+Andd i\'m glad readong your article. But want to rermark onn few general things, The website style is ideal, the articles is really 
+great : D. Good job, cheers';
+$lang['statuschanged']         = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to 
+remark on few general things, The website style is ideal, the articles 
+iss really great : D. Good job, cheers';
+$lang['wordblock']             = 'I think this is among the most important information for me.
+And i\'m glkad reading your article. But want to remark on ffew general things, The website style is ideal, the articles is 
+really great : D. Good job, cheers';
+$lang['moderation']            = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The website style is ideal, the articles is really 
+great :D. Good job, cheers';
+$lang['mail_newcomment']       = 'I think this is among tthe most important information for me.
+And i\'m glad reading your article. But want to remark on few general 
+things, The website style is ideal, the articles is really 
+gresat : D. Good job, cheers';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'I think this is among the most important information for me.
+And i\'m glad reading yoyr article. But want to remark on few general things, Thhe website style is ideal, the articles 
+is really great : D. Good job, cheers';
+$lang['status']                = 'I think this is among the most importanbt information for me.
+And i\'m glad reading your article. But want to remark on few general things, The 
+website style is ideal, the articles is really great : D.Good job, cheers';
+$lang['off']                   = 'I think this is among thee most important information for me.
+And i\'m glad reading your article. But want to remark on few general 
+things, The website style iss ideal, the articles is 
+really great : D. Good job, cheers';
+$lang['open']                  = 'I think this is among the most important information foor 
+me. And i\'m glad reading your article. But want to remark on few general things,
+The website style is ideal, the articles is really great :
+D. Good job, cheers';
+$lang['closed']                = 'I think this is among the most important information for 
+me. And i\'m glad reading your article. But want to remark on few general things, The website style 
+is ideal, the articles is really great : D. Good job, cheers';
+$lang['reply']                 = 'I think this iis among the most important 
+information for me. And i\'m glad reading your article.
+But want to remark on few general things, The website styl is ideal, the articles 
+is really great : D. Good job, cheers';
+$lang['replies']               = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But waqnt to remark on few 
+general things, The website style is ideal, the articles is really great : D.
+Good job, cheers';
+$lang['newthread']             = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few 
+general things, The website styloe is ideal, the articles is reeally great :
+D. Good job, cheers';
+$lang['toggle_display']        = 'I think this is among the most important information for me.
+And i\'m glad eading your article. But want to remark on few general 
+things, The website style is ideal, the articles is really 
+great : D. Good job, cheers';
+$lang['js']['confirmdelete']   = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few 
+general things, The website style is ideal, thee articles 
+is really great : D. Good job, cheers';
---
+++ lang/tr/settings.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author diyarbakir escort <aurelia.layton@sfr.fr>
+ */
+$lang['automatic']             = '1981';
+$lang['excluded_ns']           = 'Aurelia';
+$lang['allowguests']           = 'I think this is among the most important information for me.
+And i\'m glpad reading your article. But want to remark on few general things, The website style 
+is ideal, the articles is really great : D. Good job,cheers';
+$lang['showguests']            = 'Yes! Finally someone writes about diyarbakır escort.';
+$lang['linkemail']             = 'aurelia.layton@sfr.fr';
+$lang['useavatar']             = 'I think this is among the most important information for me.
+
+And i\'m glad reading your article. Butt want to remark on few 
+general things, The website style is ideal, the articles is 
+really great : D. Good job, cheers';
+$lang['urlfield']              = 'https://Thesocialcircles.com/story2812543/9-best-quick-fat-loss-tips-for-teenage-girls';
+$lang['addressfield']          = 'Bahnhofplatz 144';
+$lang['adminimport']           = 'Amtmerswil';
+$lang['wikisyntaxok']          = 'I think this is among thee most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The 
+website style is ideal,the articles iss really great : D.
+
+Good job, cheers';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'I thinnk this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, The website style is ideal, the articles is 
+really great : D. Good job, cheers';
+$lang['moderate']              = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But wannt to remark on few general things, 
+The website style is ideal, the articles is really great 
+: D. Good job, cheers';
+$lang['usethreading']          = 'I think this is among the most important information for me.
+
+And i\'m glad reading your article. But want too remark 
+on few general things, The website styyle is ideal, the articles is really 
+great : D. Good job, cheers';
+$lang['userealname']           = 'Aurelia Layton';
+$lang['threads_formposition']  = 'Bottom';
+$lang['threads_formposition_o_off'] = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark onn few general things,
+The website style is ideal, the articles is really great 
+: D. Good job, cheers';
+$lang['threads_formposition_o_top'] = 'I think this is among the most important information for me.
+
+And i\'m glad reading your article. But want to remark on few general things,The 
+website style is ideal, the articles iis really great : 
+D. Good job, cheers';
+$lang['threads_formposition_o_bottom'] = 'I think thios is among the most important information foor me.
+
+And i\'m glad reading youur article. Butt want to remark on few general things,The 
+website styl is ideal, the articles is really great : D.
+Good job, cheers';
+$lang['visibilityButton']      = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to rremark on few general things, The website style is ideal, tthe articles is really great : D.
+Good job, cheers';
+$lang['moderatorgroups']       = 'I think this is among the most important information for me.
+And i\'m glad reading your article. But want to remark on few general things, 
+The website style is ideal, the articles is reallly great : D.
+
+Good job, cheers';
+$lang['moderatorsnotify']      = '0';
