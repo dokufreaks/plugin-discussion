6266ef0e91e655711e47b32125fee86c6a702e53
diyarbakireskort <juliajulia@yahoo.com>
2024-06-10 13:05:26 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,122 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author diyarbakireskort <juliajulia@yahoo.com>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'We are a bunch of volunteers and opening a brand nnew scheme in our community.
+Your site provided us wiith valuable information to work on. You have performed a formidable activity and ouur whole neighborhood will probably be thankful to you.';
+$lang['direction']             = 'We aree a bunch of volunteers and opening a brand new scheme in our community.
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
+Your site provided us with valuable information too work on. You have performed a formidable activity and 
+our wholke neighborhood will probably be thankful to you.';
+$lang['menu']                  = 'We are a bunbch oof volunteers aand opening a brand new scheme 
+in our community. Youur site provided us with valuable information to work on. 
+You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['discussion']            = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable 
+activity and our whole neighborhood will probably 
+be thankful to you.';
+$lang['comment']               = 'We are a bunch of volunteers and opning a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity annd 
+our whole neighborhood will probably be thankful to you.';
+$lang['comments']              = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to woek on. You 
+have performed a formidable ativity and our whole neighborhood will probably be thankful to you.';
+$lang['nocomments']            = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have perforfmed a formidable 
+activity and our whole neighborhood will probably be thuankful to you.';
+$lang['threads']               = 'We are a bunch of volunteers and opening a brand new scheme in our community.
 
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
+Your site provided us with valuable information to wok on. You 
+have performed a formidable activity and our whole neighborhood wwill 
+probably be thankful to you.';
+$lang['url']                   = 'http://vhcvietnam.com/index.php?language=vi&nv=users&nvvithemever=t&nv_redirect=aHR0cHM6Ly9jbGFzaWZpY2Fkb3Nyb3NhcmlvLmNvbS9pbmRleC5waHA/cGFnZT1pdGVtJmlkPTI2ODg';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'We arre a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to 
+work on. You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['entercomment']          = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity and our whole neighborhood will 
+probably be thankful to you.';
+$lang['wikisyntax']            = 'We are a bunch of volunteers annd opening 
+a band new scheme in our community. Your site provided us with 
+valuable information to work on. You have performed a 
+formidable activity and our whole neighborhood will probably be thankful to 
+you.';
+$lang['older']                 = 'We are a bunch of volunteers and opening a brand new scheme in our 
+community. Your site provided us with valuable information to work on. You have performed 
+a formidable activity and our whole neighborhood will probably 
+be thankful to you.';
+$lang['newer']                 = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity annd ouur 
+whole neighborhood will probably be thankful to you.';
+$lang['btn_reply']             = 'We are a bunch of volunteers and opening a brand new scheme in oour community.
+Your site provided us with valuable information to work 
+on. You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['btn_hide']              = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site providerd us with valuable information to work 
+on. You have performed a formidable activity and our whole neighbborhood will probably be 
+thankful to you.';
+$lang['btn_show']              = 'We are a bunch of volunteers and opening a brand new scheme 
+in our community. Yoour sjte provided us with valuable information to work on. You 
+have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['btn_change']            = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activitty and our whole neeighborhood will probably be thankful to you.';
+$lang['statuschanged']         = 'We are a bunch of volunteers and opening a brand new scheme iin our community.
+Your site provided us with valuable information to work on. You have performed 
+a formidable activity and our whole neighborhood will probably 
+be thankful to you.';
+$lang['wordblock']             = 'We are a bunch of volunteers and opening a brand new scheme in our community.
 
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+Your site provided uss with valuable information to work on. 
+You have performed a formidable activity and our whole neighborhood will 
+probably be thankful to you.';
+$lang['moderation']            = 'We are a bunch of volunteers and opening a brand new scheme 
+in our community. Your site provided us with valuable information to work on. You have 
+performed a formidable activity and our whgole neighborhood will probablyy 
+be thankful to you.';
+$lang['mail_newcomment']       = 'We are a bunch of volunteers and openimg a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable actifity and our whole 
+neighborhood will probably be thankful to you.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'We are a buhnch of volunteers and opening a brand 
+new scheme in our community. Your site provided us wit valuable information to work on. You hhave perfomed a formidable activity 
+and our whole neighborhood will probably be thankful to you.';
+$lang['status']                = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided uss with valuable information to 
+work on. You have performed a formidable activity and our whole neighborhood will probably be thankful to 
+you.';
+$lang['off']                   = 'We are a bunch of volunteers annd opening a brand 
+new scheme in our community. Your site provided us with valuable information to work 
+on. You have performed a formidable activity 
+and our whole neighborhood will probably be thankful to you.';
+$lang['open']                  = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable informatikon to work on. You have performed a formidable activity and our wwhole neighborhood will probably be thankful 
+to you.';
+$lang['closed']                = 'We are a bunch of volunteers and opening a brad nnew scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity and 
+our whole neighborhood will probably be thankful to you.';
+$lang['reply']                 = 'We are a bunch of volunteers and opening a brand nnew 
+scheme in our community. Your site provieed us with valuable information to 
+work on. You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['replies']               = 'We are a buncch of volunteers and opening a brand neww scheme in our community.
+Your site provided us with valuavle information to work on. You have performed a 
+formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['newthread']             = 'We are a bunch of volunteers and opening a brand new scheme in our 
+community. Your siote provided us with valuable information to work on. You 
+have performed a formidable activikty and our whole neighborhood wil probably be thankful 
+to you.';
+$lang['toggle_display']        = 'We are a bunch of volunteers and oening a brand new scheme iin our community.
+Your site provided us with valuable information to work on. You havve performed a formidable activity and our whole 
+neighborhood will probably be thankful to you.';
+$lang['js']['confirmdelete']   = 'We are a bunch of volunteers and opening a brand new 
+scheme in our community. Your site provided us with valuable 
+information to work on. You have performed a formidable activity and our whole neighborhood will 
+probably be thankful to you.';
---
+++ lang/tr/settings.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author diyarbakireskort <juliajulia@yahoo.com>
+ */
+$lang['automatic']             = '1977';
+$lang['excluded_ns']           = 'Julia';
+$lang['allowguests']           = 'We are a bunch of volunteers and opening a brand new scheme in oour community.
+Your site provided us with valuable information to work on. 
+You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['showguests']            = 'Thank you for any other informative site. Where else may just I am getting that type of information written in such a perfect approach? I\'ve a challenge that I am just now running on, and I have been on the look out for such information.';
+$lang['linkemail']             = 'juliajulia@yahoo.com';
+$lang['useavatar']             = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['urlfield']              = 'http://vhcvietnam.com/index.php?language=vi&nv=users&nvvithemever=t&nv_redirect=aHR0cHM6Ly9jbGFzaWZpY2Fkb3Nyb3NhcmlvLmNvbS9pbmRleC5waHA/cGFnZT1pdGVtJmlkPTI2ODg';
+$lang['addressfield']          = 'Strandvej 88';
+$lang['adminimport']           = 'Kobenhavn K';
+$lang['wikisyntaxok']          = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a 
+formidable activity and our whole neighborhood will probably be thankful 
+to you.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'We are a bunch of volunteers and opening a braand new scheme 
+in our community. Your site provided us with valuable 
+information to work on. You have performed a formidable activity and our whole neighborhood will probably be thankful to 
+you.';
+$lang['moderate']              = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed 
+a formidable activity and our whole neighborhoo will probably be thankful to you.';
+$lang['usethreading']          = 'We are a bunch of volunteers and opening a brand new 
+scheme in our community. Yoour site provided uus with valuable information to work on. You 
+have performed a formidable activity and our whole neighborhood will 
+probably be thankful to you.';
+$lang['userealname']           = 'Julia Julia';
+$lang['threads_formposition']  = 'Bottom';
+$lang['threads_formposition_o_off'] = 'We are a bunch of volunteers and opening a brand new scheme in our 
+community. Your site provided us with valuable information too work on. You have performed 
+a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['threads_formposition_o_top'] = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work on. You have performed a formidable activity and our whole neighborhood will 
+probably be thankful to you.';
+$lang['threads_formposition_o_bottom'] = 'We are a bunch of volunteers and opening a brand new 
+scheme in ourr community. Your site provided us wifh valuable information to work on. 
+You have performed a formidable activity and our whole neighborhood will probably be 
+thankful to you.';
+$lang['visibilityButton']      = 'We are a bunch off volunteers and opening a brand new 
+scheme in our community. Your site prvided us with valuable information to work on. You have 
+performed a formidable activity and our whole neighborhood will probably be thankful to you.';
+$lang['moderatorgroups']       = 'We are a bunch of volunteers and opening a brand new scheme in our community.
+Your site provided us with valuable information to work 
+on. You have performed a formidable activity and our whole 
+neighborhood will probably be thankful to you.';
+$lang['moderatorsnotify']      = '0';
