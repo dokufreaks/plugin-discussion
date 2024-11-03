8338393fba25c7f52e0df217bd303e2a416250d4
DiyarbakıReskort <moisesabernathy@gmail.com>
2024-11-03 12:45:47 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,130 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author DiyarbakıReskort <moisesabernathy@gmail.com>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'Pretty section of content. I just stumbled upon your web site and in accession capital to 
+assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['direction']             = 'Pretty section of content. I just stumbled upon your web site and in accession capital to 
+asswrt that I gget in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement 
+you access consistently rapidly.';
+$lang['menu']                  = 'Pretty section of content. I just stumbled upon your web site and in accession capital tto assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribiung to your augment and 
+even I achievement you access consistently rapidly.';
+$lang['discussion']            = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that I get in fact enjoyed account your blog 
+posts. Any way I\'ll be subscribing to your augment and even I achievement you 
+access consistently rapidly.';
+$lang['comment']               = 'Pretty section of content. I just stumbled upon yokur web site 
+and iin accession capital to assert that I get in fact 
+enjoyed account your blog posts. Any wway I\'ll be subscribing to your augment and even I 
+achievement you access consistently rapidly.';
+$lang['comments']              = 'Pretty section of content. I just stumbled upon your web site and in accession capital 
+to assert that I get in fact enjoyed account your blog posts.
+Any waay I\'ll be subscribing to your augment and even I 
+achievement you access consistently rapidly.';
+$lang['nocomments']            = 'Pretty section of content. I just stumbled upon your web site 
+and in accession capital to assert that I get 
+in fact enjoyed account your bllg posts. Any way I\'ll be 
+subscribing to your augment andd even I achievement you access 
+consistently rapidly.';
+$lang['threads']               = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['url']                   = 'http://Nk-Middleeast.ae/applico/en/blog/14-3-best-ways-to-reset-windows-7-passwo';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'Pretty section of content. I just stumbled upon your web site 
+and in accession capital to assert that I get in fact enjoyed account your blog posts.
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
+Any way I\'ll be subscribing to your augment and even I achievement 
+you access consistently rapidly.';
+$lang['entercomment']          = 'Pretty section of content. I just stumbled upon your weeb site and in accession capital to assert that I gget in fact 
+enjoyed account yyour blog posts. Any way I\'ll be subscribing 
+to your augment and even I achievement yoou access consistently rapidly.';
+$lang['wikisyntax']            = 'Pretty section of content. I just stumbled upon your web site 
+and in accession capital to assert that I get in fact enjoyed account your blog 
+posts. Any way I\'ll be subvscribing to your augment andd even I 
+achievement you access consistently rapidly.';
+$lang['older']                 = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that I 
+get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing too your augment and even I achievement you access 
+consistently rapidly.';
+$lang['newer']                 = 'Pretty section off content. I just stumbled upon your web site and 
+in accession capital to assert that I gett in fact enjoyed account your blog posts.
 
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently 
+rapidly.';
+$lang['btn_reply']             = 'Preetty section of content. I just stumbled upon your wweb site and in accession capital to assert that I get in fact enjoyed account your blog posts.
 
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
+Any way I\'ll be subscribing to your augment and even I achievement 
+you access consistehtly rapidly.';
+$lang['btn_hide']              = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that I get 
+in fact enjoyed account your blog posts. Any way I\'ll be subscribing to your augment and even I achievement you 
+access consistently rapidly.';
+$lang['btn_show']              = 'Pretty section of content. Ijust stumbled upon your web site and in accession capital too assert that 
+I get in fact enjoyed accolunt your blog posts. Any way I\'ll be 
+subscribing to your augment and even I achievement 
+you access consistently rapidly.';
+$lang['btn_change']            = 'Pretty section of content. I just stumbled 
+uupon your web site and in accession capital to assert that 
+I get in fact enjoyed account your blog posts. Any way I\'ll 
+be subscribing to your augment and even I achievement you access 
+consistently rapidly.';
+$lang['statuschanged']         = 'Pretty section of content. I just stumbled upon your web site and in accession capital to 
+assert that I get in fact enjoyed account your blog posts.
 
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+Any way I\'ll be subscribing too your augment and even I achievement you access consistently rapidly.';
+$lang['wordblock']             = 'Pretty section of content. I just stumbled upon your web site and in accessioln capital to assert that I get in fact enjoyed account your 
+blog posts. Any way I\'ll be subsccribing to your augment andd even I 
+achievement you access consistently rapidly.';
+$lang['moderation']            = 'Pretty section of content. I just stumbled upon your web site 
+and in accession capital to assert that I get 
+in fact enjoyed account your blog posts. Any way I\'ll be subscribing to your augment and even I 
+achievement you access consistently rapidly.';
+$lang['mail_newcomment']       = 'Pretty section of content. I just stumbled upon your web site and in accession capital to 
+assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you 
+access consistently rapidly.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['status']                = 'Pretty section of content. I just stumbled upon your web site and in accession capital too assert 
+that I get in fact enjoyed account your blog posts.
+
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['off']                   = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert that 
+I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augmenht and even I 
+aachievement you access consistently rapidly.';
+$lang['open']                  = 'Pretty section of content. I just stumbled upon your web site and in accession cqpital to assert that I gett in fact enjoyed account your blog posts.
+
+Any way I\'ll be subscribing to your augment and even I 
+achievement you access consistently rapidly.';
+$lang['closed']                = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert thawt 
+I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['reply']                 = 'Pretty section of content. I just stumbled upon your web site andd inn accession capital to assert that I get in fact enjoyed 
+account your blog posts. Any way I\'ll be subscribing to 
+your augment and even I achievement you access 
+consistently rapidly.';
+$lang['replies']               = 'Pretty section off content. I just stumbled upon youjr web site and 
+in accession capital to assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['newthread']             = 'Pretty section of content. I just stumbled upon your web site and in accession capital to assert 
+that I get in fact enjoyed account your blog posts. Any way I\'ll be subscribing to 
+your augment and even I achievement you access consistently 
+rapidly.';
+$lang['toggle_display']        = 'Pretty section of content. I just stumbled upon your web site and 
+in accession capital to asert that I get in fact enjoyed account your 
+blog posts. Any way I\'ll be subscribing to your augment and even I achievement you access consistently rapidly.';
+$lang['js']['confirmdelete']   = 'Pretty section of content. I just stumbled upon your web site and in accession capital to 
+assert that I get in fact enjoyed account your blog posts.
+Any way I\'ll be subscribing to your augment and even I achievement 
+you access consistently rapidly.';
