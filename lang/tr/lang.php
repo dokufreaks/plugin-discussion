a85ff11402649670277662a006fb9382cf132806
DiyarbakıR Ofis Eskort <meagangoodrich@orange.fr>
2024-11-15 21:55:07 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,104 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author DiyarbakıR Ofis Eskort <meagangoodrich@orange.fr>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'Verry nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+After alll I will be subscribing in your feed and I hope you write again soon!';
+$lang['direction']             = 'Very nice post. I just stumbled upon your blog 
+and wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscriing in your feed and I hope you write again soon!';
+$lang['menu']                  = 'Very nice post. I just stumbled upon your blog and wanted to say that Ihave truly loved surfing around yur weblog posts.
+After all I will be subscribing in your feed and I hope yoou write again soon!';
+$lang['discussion']            = 'Very nice post. I just stumbled upon your blog and wantged to say 
+that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your fewd and I hope you write again soon!';
+$lang['comment']               = 'Very nice post. I just stumbled upon your blog and wanted 
+to say that I have truly loved surfing around your weblog 
+posts. After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['comments']              = 'Very nice post. I just stumbled upon your blog and wanted to say hat I have truly 
+loved surfing around your weblog posts. After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['nocomments']            = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['threads']               = 'Very nice post. I just stumbled upon your blog and wanted to say tbat I haave truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['url']                   = 'https://Gocamp.deb.kr/bbs/board.php?bo_table=free&wr_id=1595106';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'Very nice post. I just stumbled upon your blog and wated tto say that I hace truly loved surfing around 
+your weblog posts. After all I will be subscribing in your feed and 
+I hope youu write again soon!';
+$lang['entercomment']          = 'Very nice post. I just stumbled upo yokur blog and wanted to say that I have truly loved surfing 
+around your weblog posts. After all I will be subscribing 
+in your feed and I hope you write again soon!';
+$lang['wikisyntax']            = 'Very nife post. I just stumbled uupon your blog and wanted 
+to say that I have truly lloved surfing around your weblog posts.
+After all I wull be suubscribing in your feed and I hope you write again soon!';
+$lang['older']                 = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['newer']                 = 'Very nice post. I just stumbled upon your blog and wanted 
+to say that I have truly loved surfing around ykur weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['btn_reply']             = 'Very nice post. I just stumbled upon your blog and wanted too say that I 
+have truly loved surfing around your weblog posts. After all I will be subscribing in your feed and I 
+hope you write again soon!';
+$lang['btn_hide']              = 'Very nice post. I just stumbled upon your blog and wanted to say that 
+I have truly loved surfing around your weblog posts.
+After all I will bbe subscribing in your feed and I hope youu 
+write again soon!';
+$lang['btn_show']              = 'Very nice post. I just stumbled upon your blog and wanted to say that 
+I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['btn_change']            = 'Very nice post. I just stumbled upon your blog and wanted to say 
+that I have truly loved surfving around 
+your weblog posts. After all I will be subscribing in your feed and I 
+hope you write again soon!';
+$lang['statuschanged']         = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+Afteer all I will be subscribing in your feed and I 
+hope you write again soon!';
+$lang['wordblock']             = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing 
+around your weblog posts. After all I will be subscribing in your feed and I 
+hope you write again soon!';
+$lang['moderation']            = 'Very nice post. I just stumbled upon your blog and wanted to say that I havbe 
+truly loved surfing around your weblog posts. After all I will 
+be subscribing in your feed and I hope you write again soon!';
+$lang['mail_newcomment']       = 'Very nice post. I just stumbled upon youir blog and wanted to say that I have 
+truly loved surfing around your weblog posts. After all I will be 
+subscribing in your feed and I hope youu write again soon!';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Very nice post. I just stumbled upon your blog and 
+wanted to say that I have truly loved surfing 
+around your weblog posts. After all I will be subscribing in your 
+feed and I hope you write again soon!';
+$lang['status']                = 'Veryy nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
-
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
-
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['off']                   = 'Veryy nice post. I just stumbled upon youhr blog and wanted to say that I hsve trily loved 
+surfing around your weblog posts. After all I will be subscribing 
+in your feed and I hope you write again soon!';
+$lang['open']                  = 'Very nice post. I just stumbled upon your blog and wanted to say 
+that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['closed']                = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['reply']                 = 'Very nice post. I just stumbled upon your blog and wanted to say that I have 
+truly loved surfing around your weblog posts. After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['replies']               = 'Very nice post. I just stumbled upon your blog aand wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your fesd and I hoe you write 
+again soon!';
+$lang['newthread']             = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write 
+again soon!';
+$lang['toggle_display']        = 'Very nice post. I just stumbled upon your blog and wanted to say that 
+I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed 
+and I hope you write again soon!';
+$lang['js']['confirmdelete']   = 'Very nice post. I just stumbled upon your blog and wanted 
+to say thhat I have truly loved surfing around your weblog posts.
+After all I will bbe subscribing in your feed and I hope you write again soon!';
---
+++ lang/tr/settings.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author DiyarbakıR Ofis Eskort <meagangoodrich@orange.fr>
+ */
+$lang['automatic']             = '1987';
+$lang['excluded_ns']           = 'Meagan';
+$lang['allowguests']           = 'Very nice post. I just stumbled upon your blog and wanted tto say 
+that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope yoou write again soon!';
+$lang['showguests']            = 'Hi to all, the contents present at this site are truly amazing for people knowledge, well, keep up the good work fellows.';
+$lang['linkemail']             = 'meagangoodrich@orange.fr';
+$lang['useavatar']             = 'Very nice post. I just stumbled upoon your blog and wanted to sayy 
+that I have truly loved urfing around your weblog posts. After all 
+I will be suubscribing in your feed and I hope you write again soon!';
+$lang['urlfield']              = 'https://Gocamp.deb.kr/bbs/board.php?bo_table=free&wr_id=1595106';
+$lang['addressfield']          = 'Eikstraat 444';
+$lang['adminimport']           = 'Beigem';
+$lang['wikisyntaxok']          = 'Very nice post. I jst stumbled upon your blog and wanted to say thqt I 
+have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Veryy nice post. I just stumbbled upon your blog and wanted to say that I have 
+truly loved surfing around your weblog posts. After all I will be subscribing in your 
+feed and I hope you write again soon!';
+$lang['moderate']              = 'Very nice post. I just stumbled upon your blog and wanted to say that I have 
+truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed andd 
+I hope you write again soon!';
+$lang['usethreading']          = 'Very nice post. I just stumbled upon your blog and wanted to say that I 
+have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['userealname']           = 'Meagan Goodrich';
+$lang['threads_formposition']  = 'position of the new thread form shown by the {{threads> }} syntax';
+$lang['threads_formposition_o_off'] = 'Very nice post. I just stumbled upon your blog and wanted 
+to say that I have truly loved surfing around your weblog posts.
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['threads_formposition_o_top'] = 'Very nice post. I just stumbled upon your blog and wanted to say that 
+I have truly loved surfing around your weblog posts.
+After alll I will be subscribing in your feed and I hope you write again soon!';
+$lang['threads_formposition_o_bottom'] = 'Very nnice post. I just stumbled upon your blog and wanted to say that I have truly loved surfing around your weblog posts.
+
+After all I will be subscribing in your feed and I hope you write again soon!';
+$lang['visibilityButton']      = 'Very nice post. I just stumbled upon your blog and wanted to say that I have truly loved 
+surfing around your weblog posts. After all I will be subscribing in your feed and I hope you write 
+again soon!';
+$lang['moderatorgroups']       = 'Very nice post. I just stumbled upon your blog and wanted to say that I have 
+ttruly loved surfing around your weblog posts. After all 
+I will be subscribing in your feed and I hope you write again soon!';
+$lang['moderatorsnotify']      = '0';
