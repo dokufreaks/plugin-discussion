752dda7ac61d1f9fc8872150e016168da4a7f054
DiyarbakıR Escort Bayan <adamyost@gmail.com>
2025-01-04 09:35:48 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,134 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author DiyarbakıR Escort Bayan <adamyost@gmail.com>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkmbox and noww each time a comment is added I get four emails with the same comment.
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
+Is there any way you can remove people from that service? Thank you!';
+$lang['direction']             = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['menu']                  = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I 
+get four emails with the same comment. Is there any way you 
+caan emove people from that service? Thank you!';
+$lang['discussion']            = 'When I initiaoly commented I clicked the "Notify me when new comments are added" checkbox and now each time a 
+comment is addwd I get four emails with the same comment.
+Is there any way you can remove people from that service?
 
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
+Thank you!';
+$lang['comment']               = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with thhe 
+same comment. Is there any way you can remove people from that service?
+Thank you!';
+$lang['comments']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now 
+each time a comment is added I get for emails with 
+the same comment. Is there any way you can remove people from that service?
 
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
+Thank you!';
+$lang['nocomments']            = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment 
+is added I get four emils with the same comment. Is there any way you can remove people from that 
+service? Thank you!';
+$lang['threads']               = 'Whenn I initially commented I clicked the "Notify me when new comments are added" cgeckbox and now each time a comment is added I get four emails with the same comment.
+Is there any wway you can remove people from that service?
+Thank you!';
+$lang['url']                   = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7M.Nb.V.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://Penzu.com/p/fb6f130a2195c8bb';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time 
+a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['entercomment']          = 'When I initially commented Iclicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails ith the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['wikisyntax']            = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and 
+now each time a comment iss added I get four emails with 
+the same comment. Is there any way you can remove people from that service?
+Thank you!';
+$lang['older']                 = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now eaach time a comment is added I get four emails 
+with the same comment. Is there any way you can remove people from that service?
+Thank you!';
+$lang['newer']                 = 'When I initially commented I clicked the "Notify me when new comments are added" chewckbox 
+and now each time a comment is added I get 
+four emails wiuth the same comment. Is there any way you can remove people from that 
+service? Thank you!';
+$lang['btn_reply']             = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and 
+now each time a comment is aded I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['btn_hide']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with the same comment.
+Is there any wway you can remove people from that service?
+Thank you!';
+$lang['btn_show']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a 
+comment is added I get four emails with the saame comment.
 
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+Is there any way you can remove people frrom that service?
+
+Thank you!';
+$lang['btn_change']            = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each 
+time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['statuschanged']         = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and 
+now each time a comment is added I get four emails ith the same comment.
+Is there any way you can remove people from that service?
+
+Thank you!';
+$lang['wordblock']             = 'When I initially commented I cicked the "Notify me when new comments are added" 
+checkbox and now each time a comment is added 
+I get four emails with the same comment. Is there any way you 
+can remove people from that service? Thank you!';
+$lang['moderation']            = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment iss added 
+I get for emails with the same comment. Is there any way yyou can remove people 
+from that service? Thank you!';
+$lang['mail_newcomment']       = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with thee same comment.
+Is there any way you can remove people from that service?
+
+Thanjk you!';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added 
+I get four emails with the sake comment. Is there any way you 
+can remove people from that service? Thank you!';
+$lang['status']                = 'When I initially commented I clicked the "Notify me when new comments are added" 
+checkbox and now each time a comment is added I get four emails with 
+the same comment. Is there any way you can remove people from that 
+service? Thank you!';
+$lang['off']                   = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and 
+now each time a comment is added I get four emails ith the same comment.
+
+Is there any way you can remove people from that service? Thank you!';
+$lang['open']                  = 'Whhen I initially commented I clicked the "Notify me when new comments are added" checkbox 
+and now each time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['closed']                = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox aand now each time a 
+comment is added I get four emails with thhe same comment.
+Is there any way youu can remove people from that service?
+Thank you!';
+$lang['reply']                 = 'When Iinitially commented I clicked the 
+"Notify me when new comments are added" checkbox and now each time a comment is added I get four 
+emails with the same comment. Is there any way you can remove people from 
+that service? Thank you!';
+$lang['replies']               = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with the same 
+comment. Is there any way you can remove people from that service?
+Thank you!';
+$lang['newthread']             = 'When I initially commented I clicked the "Notify me when new comments are added" 
+checkbox andd now each time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['toggle_display']        = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox 
+and now each time a comment is added I get four emails 
+with the same comment. Is there any way you can remove people from that service?
+
+Thank you!';
+$lang['js']['confirmdelete']   = 'When I initially commented I clicked the "Notify me when new comments are added" 
+checkbox and now each time a comment is added I get four emails with the 
+same comment. Is there any way you can remove people from hat service?
+
+Thank you!';
---
+++ lang/tr/settings.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author DiyarbakıR Escort Bayan <adamyost@gmail.com>
+ */
+$lang['automatic']             = '1979';
+$lang['excluded_ns']           = 'Adam';
+$lang['allowguests']           = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each 
+time a comment is added I get four emails with the same comment.
+Is theree any way you can remove people from that 
+service? Thank you!';
+$lang['showguests']            = 'Hi, this weekend is good designed for me, because this time i am reading this fantastic informative paragraph here at my house.';
+$lang['linkemail']             = 'adamyost@gmail.com';
+$lang['useavatar']             = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that 
+service? Thank you!';
+$lang['urlfield']              = 'http://P.L.A.U.Sible.L.J.H@I.N.T.E.Rloca.L.Qs.J.Y@trsfcdhf.hfhjf.hdasgsdfhdshshfsh@hu.fe.ng.k.ua.ngniu.bi..uk41@Www.Zanele@silvia.woodw.o.r.t.h@Shasta.ernest@ba.tt.le9.578@jxd.1.4.7M.Nb.V.3.6.9.cx.z.951.4@Ex.p.lo.si.v.edhq.g@silvia.woodw.o.r.t.h@r.eces.si.v.e.x.G.z@leanna.Langton@blank.e.tu.y.z.s@m.i.scbarne.s.w@e.xped.it.io.n.eg.d.g@burton.rene@e.xped.it.io.n.eg.d.g@burton.rene@Gal.EHi.Nt.on78.8.27@dfu.s.m.f.h.u8.645v.nb@WWW.EMEKAOLISA@carlton.theis@silvia.woodw.o.r.t.h@s.jd.u.eh.yds.g.524.87.59.68.4@Sus.ta.i.n.j.ex.k@www.mondaymorninginspiration@n.i.gh.t.m.a.re.zzro@hygiene.gb.n.z@e.c.d.ftvghujihjb.hsndgskdjbslkged@beatriz.mcgarvie@j.o.r.n.s.tory@jo.hnsdfsdff.dsgdsgdshdghsdhdhfd@Obtainable.Brakeobscenefriendse@J.U.Dyquny.Uteng.Kengop.Enfuyuxen@Www.Syb3Er.Eces.Si.V.E.X.G.Z@Leanna.Langton@Sus.Ta.I.N.J.Ex.K@Hu.Fen.Gk.Uang.Ni.U.B.I.xn--.U.K.6.2@2ch-ranking.net/redirect.php?url=https://Penzu.com/p/fb6f130a2195c8bb';
+$lang['addressfield']          = 'Avenida Gramado 569';
+$lang['adminimport']           = 'Foz Do Iguacu';
+$lang['wikisyntaxok']          = 'Whenn I initially commented I clicked the "Notify me when new comments are added" checkbox and now ach time a comment is added I get 
+four emails with tthe same comment. Is there any way you can remove people from that service?
+Thank you!';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each tim 
+a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['moderate']              = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now 
+each tme a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['usethreading']          = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each 
+time a comment is added I get four emails with the samje comment.
+Is there any way you can remove people from that service? Thank you!';
+$lang['userealname']           = 'Adam Yost';
+$lang['threads_formposition']  = '{{threads>new}}';
+$lang['threads_formposition_o_off'] = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now 
+each time a comment is added I get four emails with the same comment.
+Is there any way you can remove people from that service? Thank you!';
+$lang['threads_formposition_o_top'] = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment 
+is added I get four emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['threads_formposition_o_bottom'] = 'When I initially commented I clicked the "Notify me when new comments are added" 
+checkbox and now each time a comment is added I get fouhr emails with the same comment.
+Is there any way you can remove people from that service?
+Thank you!';
+$lang['visibilityButton']      = 'When I initially commented I clicked the "Notify me when new comments are added" checkbox 
+and now each time a comment is added I get four emails with the same 
+comment. Is there any way yoou can remove people from that service?
+Thank you!';
+$lang['moderatorgroups']       = 'Whenn I initially commented I clicked the "Notify me when new comments are added" checkbox and now each time a comment is added I get four emails with the same comment.
+Is tuere any way you can reove people from 
+that service? Thank you!';
+$lang['moderatorsnotify']      = '0';
