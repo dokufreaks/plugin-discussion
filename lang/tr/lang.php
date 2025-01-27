03071f8ed15b298c7cd1c7bdb9139f5db3afdfc3
Diyarbakır Escort <maudehardman@yahoo.fr>
2025-01-27 01:55:48 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,80 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author Diyarbakır Escort <maudehardman@yahoo.fr>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
-
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
-
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
-
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['encoding']              = 'Actuaoly no matter if spmeone doesn\'t be aware of afterward its up to other visitors that they will 
+help, so here it occurs.';
+$lang['direction']             = 'Actually nno matter if someone doesn\'t be aware of afterward its up to other 
+visitors that they will help, so here itt occurs.';
+$lang['menu']                  = 'Actually no matter if someone doesn\'t be aware off afterward its 
+up to other viisitors that they will help, so here it occurs.';
+$lang['discussion']            = 'Actually no matter if someone doesn\'t be aware of afterward its up to other vissitors that they will 
+help, so here it occurs.';
+$lang['comment']               = 'Actually nno matter if someone doesn\'t be aware of afterward its 
+up to other visitors that they will help, so hsre it occurs.';
+$lang['comments']              = 'Actually no matter if someone doesn\'t be aware of afterward its up to 
+other visitors thst they will help, so here it occurs.';
+$lang['nocomments']            = 'Actually no matter if someone doesn\'t be aware of afterward itts up to other visitors that they will help, so here it occurs.';
+$lang['threads']               = 'Actually no matter if someone doesn\'t be aware of afterward its up to other 
+visitors that they will help, so here it occurs.';
+$lang['url']                   = 'http://Koleyjessenfirm.com/__media__/js/netsoltrademark.php?d=10.Glawandius.com%2Findex%2Fd1%3Fdiff%3D0%26utm_source%3Dogdd%26utm_campaign%3D26607%26utm_content%3D%26utm_clickid%3Dsnqcg0skg8kg8gc0%26aurl%3Dhttp%253A%2F%2Fmedium.com%252F%2540bahosero560%252Fdiyarbak%25C4%25B1r-escort-hizmetleri-c2c29ab969af%26source%3Dog%26campaign%3D4397%26content%3D%26clickid%3Dhrx9nw9psafm4g9v%26title%3Djoellemonetcream99964%26url%3Dhttps%253A%2F%2Fjoellemonet.com%2F%26email%3Djettmcguigan%2540web.de';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'Actually no matter iif someone doesn\'t be aware of afterward its 
+up to othber visitors that they will help, so here it occurs.';
+$lang['entercomment']          = 'Actually no matter if someone doesn\'t be aware of affterward its up 
+to other visitors that they will help, so here it occurs.';
+$lang['wikisyntax']            = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors 
+that they will help, sso here it occurs.';
+$lang['older']                 = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors that they 
+will help, sso here it occurs.';
+$lang['newer']                 = 'Actually no matter if someone doesn\'t be aware of afterward its upp to other visitors 
+that they will help, so here it occurs.';
+$lang['btn_reply']             = 'Actually no matter if someone doesn\'t be aware 
+of afterward its up to other visitors that they will help, so here it occurs.';
+$lang['btn_hide']              = 'Actually no matter if someone doesn\'t be aware of afterward 
+its up to other visitors that they will help, so here 
+it occurs.';
+$lang['btn_show']              = 'Actually no matterr if someone doesn\'t be aware of aftsrward its up to othuer visitors that 
+they will help, so here it occurs.';
+$lang['btn_change']            = 'Actually no matter if someone doesn\'t be aware 
+of afterward its up to other visitors that they 
+will help, so here it occurs.';
+$lang['statuschanged']         = 'Actually no matter if someone doesn\'t be aware of afterward itts up too other visitors that 
+they will help, so here it occurs.';
+$lang['wordblock']             = 'Actually no matter if someone doesn\'t bee aware of afterward 
+its up to other visitors that they will help, so here it 
+occurs.';
+$lang['moderation']            = 'Actually no atter if someone doesn\'t be aware of afterward its up to other 
+visitors tht they will help, so here it occurs.';
+$lang['mail_newcomment']       = 'Actually no matter if someone doesn\'t be aware of 
+afterward its up to other visitors that they will help, 
+so here it occurs.';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors 
+that they will help, so here it occurs.';
+$lang['status']                = 'Actually no matter if someone doesn\'t be aware of afterward itss up to other visitors that they will help, 
+so here it occurs.';
+$lang['off']                   = 'Actually no matter if someone doesn\'t bee aware of afterward its up to other visitors 
+that they will help, so here it occurs.';
+$lang['open']                  = 'Actfually no matter if someone doesn\'t be 
+aware off afterward its uup to other visitors that they will help, 
+so here it occurs.';
+$lang['closed']                = 'Actually no mtter if someone doesn\'t be aware of afterward its up to other visitors 
+that they will help, so here it occurs.';
+$lang['reply']                 = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors that they will help, so here 
+it occurs.';
+$lang['replies']               = 'Actually no matter if someone doesn\'t be aware of afterward its 
+up to other visitors that they will help, so here it occurs.';
+$lang['newthread']             = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors that they will help, so here it occurs.';
+$lang['toggle_display']        = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors that they will help, so here it occurs.';
+$lang['js']['confirmdelete']   = 'Actually no matter if someone doesn\'t be awarde of 
+afterward its upp to other visitors that they will help, so 
+here it occurs.';
---
+++ lang/tr/settings.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Diyarbakır Escort <maudehardman@yahoo.fr>
+ */
+$lang['automatic']             = '1990';
+$lang['excluded_ns']           = 'Maude';
+$lang['allowguests']           = 'Actually no mattesr if someone doesn\'t bee aware of afterward its 
+up to other visitors that they will help, so here it occurs.';
+$lang['showguests']            = 'At this time I am going to do my breakfast, after having my breakfast coming yet again to read further news.';
+$lang['linkemail']             = 'maudehardman@yahoo.fr';
+$lang['useavatar']             = 'Actually no matter if someone doesn\'t be aware 
+of afterward its uup to other visitors that they will help, so 
+here it occurs.';
+$lang['urlfield']              = 'http://Koleyjessenfirm.com/__media__/js/netsoltrademark.php?d=10.Glawandius.com%2Findex%2Fd1%3Fdiff%3D0%26utm_source%3Dogdd%26utm_campaign%3D26607%26utm_content%3D%26utm_clickid%3Dsnqcg0skg8kg8gc0%26aurl%3Dhttp%253A%2F%2Fmedium.com%252F%2540bahosero560%252Fdiyarbak%25C4%25B1r-escort-hizmetleri-c2c29ab969af%26source%3Dog%26campaign%3D4397%26content%3D%26clickid%3Dhrx9nw9psafm4g9v%26title%3Djoellemonetcream99964%26url%3Dhttps%253A%2F%2Fjoellemonet.com%2F%26email%3Djettmcguigan%2540web.de';
+$lang['addressfield']          = 'Binzmuhlestrasse 141';
+$lang['adminimport']           = 'Salmsach';
+$lang['wikisyntaxok']          = 'Actually no matter if someone doesn\'t be aware of afterward its 
+up to othr visitors that they wikl help, so here 
+it occurs.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'Actually no mattr if someone doesn\'t bee aware of afterward its up to other visitors that 
+they will help, so here it occurs.';
+$lang['moderate']              = 'Actually no matter iif someone doesn\'t be aware of 
+afterward its up to other visitors that they will help, so here it occurs.';
+$lang['usethreading']          = 'Actually no matter if someone doesn\'t be aware of afterward its up to 
+other visitors that they will help, soo here it occurs.';
+$lang['userealname']           = 'Maude Hardman';
+$lang['threads_formposition']  = 'position of the new thread form shown by the {{threads> }} syntax';
+$lang['threads_formposition_o_off'] = 'Actually no matter if someone doesn\'t be aware of afterward its 
+up to other visitors that they will help, so here it 
+occurs.';
+$lang['threads_formposition_o_top'] = 'Actually no matter if someone doesn\'t be aware of afterward its up 
+to other visitors that they will help, so here it occurs.';
+$lang['threads_formposition_o_bottom'] = 'Actually no matter if someone doesn\'t be aware of afterward its up to other visitors 
+that they will help, so here it occurs.';
+$lang['visibilityButton']      = 'Actually noo matter if someone doesn\'t be aware of afterward its 
+up to other visitors that they will help, so here it occurs.';
+$lang['moderatorgroups']       = 'Actuall no matter if someone doesn\'t be aware of afterward its up to other visitors that they will help, so here it 
+occurs.';
+$lang['moderatorsnotify']      = '0';
