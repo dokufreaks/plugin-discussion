e1ba0629ebc2e37414a0499fff6201214242b019
diyarbakir Escort <fernwhiteman@libero.it>
2025-02-23 20:05:07 +0100

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author diyarbakir Escort <fernwhiteman@libero.it>
+ */
+$lang['automatic']             = '1983';
+$lang['excluded_ns']           = 'Fern';
+$lang['allowguests']           = 'It\'s going to be ending of mine day, however before end I am reading this enormous piece of writing too increase my 
+knowledge.';
+$lang['showguests']            = 'I real lucky to find this website on bing, just what I was looking for :D too saved to my bookmarks.';
+$lang['linkemail']             = 'fernwhiteman@libero.it';
+$lang['useavatar']             = 'It\'s going to be ending of mine day, however before end I 
+am reading this enormous piece of writing to increase my knowledge.';
+$lang['urlfield']              = 'https://www.naseb.com/@freemanmusser';
+$lang['addressfield']          = 'Oldesloer Strasse 55';
+$lang['adminimport']           = 'Goldisthal';
+$lang['wikisyntaxok']          = 'It\'s gling to be ending of mine day, however before end 
+I am reading this enormous piece of writing to increase my knowledge.';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'It\'s going to be ending of mine day, however 
+before end I am reading this enormous piece of writing to increase my knowledge.';
+$lang['moderate']              = 'It\'s going to be eending of mine day, however before end I am reading this enormous piece of writing to increase my knowledge.';
+$lang['usethreading']          = 'It\'s going to be ending of mine day, however before end I 
+am reading this enormous piece of writing to increase my knowledge.';
+$lang['userealname']           = 'Fern Whiteman';
+$lang['threads_formposition']  = 'position of the new thread form shown by the {{threads> }} syntax';
+$lang['threads_formposition_o_off'] = 'It\'s going to be ebding of mine day, however 
+before end I am reading this enormous piece of writing to increase myy knowledge.';
+$lang['threads_formposition_o_top'] = 'It\'s going to be ending of mine day, however before end I am reading this enormous piece of 
+writing to increase my knowledge.';
+$lang['threads_formposition_o_bottom'] = 'It\'s going to be ending of mine day, however before endd I am reading 
+this enormous piece of writing to increase my knowledge.';
+$lang['visibilityButton']      = 'It\'s going to be ending of mine day, however before end I am reading this enormous piece of writing to increase my knowledge.';
+$lang['moderatorgroups']       = 'It\'s goig to be ending of mine day, however before end I 
+am reading this enormous piece of writing to increase my knowledge.';
+$lang['moderatorsnotify']      = '0';
