b302de5da703af357e31a00d6ff16062c7a34864
Diyarbakir Gecelik Escort Hizmeti <frankie.love@yahoo.com>
2025-08-08 02:00:07 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -1,51 +1,90 @@
 <?php
+
 /**
  * Turkish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <wikidesign@gmail.com>
- * @author     iDo <iLoveiDo@gmail.com>
  * Translation İlker R. Kapaç <irifat@gmail.com>
+ *
+ * @author Diyarbakir Gecelik Escort Hizmeti <frankie.love@yahoo.com>
+ * @author Esther Brunner <wikidesign@gmail.com>
+ * @author iDo <iLoveiDo@gmail.com>
  */
- 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu']            = 'Tartışmaları Yönet'; 
+$lang['encoding']              = 'I want to to thank you for this good read!! I definitely loved every bit of it.
+I have got you book-marked to check out neew stuff you post…';
+$lang['direction']             = 'I want to to thank you for this good read!! I definitely loved every 
+bbit of it. I have got yoou book-marked to check out new stuff you post…';
+$lang['menu']                  = 'I want to to thank you ffor this good read!! I definitely 
+loved every bit of it. I have got you book-marked to check 
+out new stuff you post…';
+$lang['discussion']            = 'I want tto to thank you for this good read!! I definitely loved evvery bit of it.
+I have got you book-marked to check out new stuff you post…';
+$lang['comment']               = 'I want to to thank you for this good read!! I definitely loved every bit of it.I have 
+got you book-marked to check out new stuff you post…';
+$lang['comments']              = 'I want to to thank you for this good read!!
 
-$lang['discussion']      = 'Tartışma';
-$lang['comment']         = 'Yorum';
-$lang['comments']        = 'Yorumlar';
-$lang['nocomments']      = 'Yorumlar';
-$lang['url']             = 'İnternet sitesi';
-$lang['address']         = 'Adres';
-$lang['date']            = 'Tarih';
-$lang['entercomment']    = 'Yorumunuzu girin';
-$lang['wikisyntax']      = 'Wiki sözdizimi kullanabilirsiniz';
-$lang['older']           = 'Eski tartışmalar';
-$lang['newer']           = 'Yeni tartışmalar';
-$lang['btn_reply']       = 'Yanıtla';
-$lang['btn_hide']        = 'Gizle';
-$lang['btn_show']        = 'Göster';
-$lang['btn_change']      = 'Değiştir';
-$lang['statuschanged']   = 'Tartışma durumu değiştirildi';
-$lang['wordblock']       = 'Yorumunuz spam olarak engellendi.';
-$lang['moderation']      = 'Yorumunuz kaydedildi. Yayınlanması için onay bekliyor';
-$lang['mail_newcomment'] = 'Yeni Yorum';
-$lang['subscribe']       = 'Tartışmaya abone ol';
-$lang['noguests']        = 'Yorum yazabilmek için sisteme giriş yapmış olmalısınız.';
+I definitely loved every bit of it. I have got you 
+book-marked to check out neew stuff you post…';
+$lang['nocomments']            = 'I want to to thank you for this good read!! I definitely loved every bit of it.
+I have got yyou book-marked to check out new stuff you post…';
+$lang['threads']               = 'I want to to thank you for thks good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out neew stuff you post…';
+$lang['url']                   = 'http://Slnk.info/v8nre';
+$lang['address']               = 'Adres';
+$lang['date']                  = 'I want to to thank you for this good read!! I definitely 
+oved every bit of it. I have got you book-marked to chneck out new 
+stuff yyou post…';
+$lang['entercomment']          = 'I want to to thank you for this good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out new sturf you post…';
+$lang['wikisyntax']            = 'I want to too thank you for this good read!! I definitely 
+loved every bit of it. I have got you book-marked to check 
+out new stuff you post…';
+$lang['older']                 = 'I want to to thank you for this good read!!
+I definitely loved every bit of it. I have got you book-marked to check out new 
+stuff you post…';
+$lang['newer']                 = 'I want to to thank you for this good read!! I definitely 
+loved every bit of it. I have got you book-marked to check out new stuff yyou post…';
+$lang['btn_reply']             = 'I want to to thank you for this good read!!
+I definitely loved every bit of it. I have got you book-marked to check out new stuff you post…';
+$lang['btn_hide']              = 'I want to tto thank you for this good read!! I definitely loved every bitt of it.
+I have got you book-marked to check out new stuff 
+you post…';
+$lang['btn_show']              = 'I want to to thank you for this good read!!
+I defiknitely loved every bit of it. I have got you book-marked to check out new stuff you post…';
+$lang['btn_change']            = 'I want to to thank you for this good read!! I definitely loved 
+every bit of it. I have got you book-marked to check out new stuff you post…';
+$lang['statuschanged']         = 'I want to to thank you for this good read!! I definitely loved every bit of it.
+I have got you book-marked to check out new stuff you post…';
+$lang['wordblock']             = 'I want tto to thank you for this good read!! I definitely loved every bit of 
+it. I have got you book-marked to check out new stuff 
+you post…';
+$lang['moderation']            = 'I want to to thank you for this good read!! I defihitely loved every 
+bit of it. I havee got you book-marked to check ouut new stuff you post…';
+$lang['mail_newcomment']       = 'I want to to thank you for this good read!! I definitely loved every bit oof it.
+I have got you book-marked to check out neew stuff you post…';
+$lang['subscribe']             = '0';
+$lang['noguests']              = 'Iwant to to thank you for this good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out new stuff you 
+post…';
+$lang['status']                = 'I want to to thank you for this good read!! I definitely loved every bit off it.
+I have got you book-marked to check out new stuff you post…';
+$lang['off']                   = 'I want to to thank youu for this good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out new stuff youu post…';
+$lang['open']                  = 'I want to to thank you for this good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out new stuff you post…';
+$lang['closed']                = 'I want to to thank you for this good read!!
+I definitely loved every bit of it. I have got you book-marked to check outt new stuff you post…';
+$lang['reply']                 = 'I want to to thajk you for this good read!! I definitely loved every bit of it.
+I have got you book-marked to chsck out new stuff you post…';
+$lang['replies']               = 'I want to to thank you for this goodd read!! I definitely loved every bit of it.
 
-$lang['status']          = 'Durum';
-$lang['off']             = 'kapalı';
-$lang['open']            = 'açık';
-$lang['closed']          = 'kapatılmış';
+I have got you book-marked to check out new stuff 
+you post…';
+$lang['newthread']             = 'I want to to thank you forr this good read!! I definitely loved every 
+bit oof it. I have got you book-marked to check out new stuff you post…';
+$lang['toggle_display']        = 'I want to to thank you for this good read!! I definitely loved 
+every bit of it. I have got you book-marked to check out 
+new stuff you post…';
+$lang['js']['confirmdelete']   = 'I want to to thank you for this good read!! I definitely loved every bit of it.
 
-$lang['reply']           = 'Yanıt';
-$lang['replies']         = 'Yanıtlar';
-$lang['newthread']       = 'Yeni tartışma sayfası';
-$lang['toggle_display']  = 'Tartışmayı Gizle/Göster';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+I have got you book-marked to check out new stuff you post…';
---
+++ lang/tr/settings.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Diyarbakir Gecelik Escort Hizmeti <frankie.love@yahoo.com>
+ */
+$lang['automatic']             = '1980';
+$lang['excluded_ns']           = 'Frankie';
+$lang['allowguests']           = 'I want to to thank you for this good read!! I definitely loved every bit of it.
+I have got you book-marked to check outt new stuff you post…';
+$lang['showguests']            = 'I conceive this web site has some rattling wonderful information for everyone :D.';
+$lang['linkemail']             = 'frankie.love@yahoo.com';
+$lang['useavatar']             = 'I want to to thank you for this good read!!
+I definitely loved every bit of it. I have got you book-marked tto check out new stuff you post…';
+$lang['urlfield']              = 'http://Slnk.info/v8nre';
+$lang['addressfield']          = 'Ul. Anyzkowa 147';
+$lang['adminimport']           = 'Warszawa';
+$lang['wikisyntaxok']          = 'I want to to thank you for this good read!! I definitely loved every bit 
+of it. I have got you book-marked to check out new stuff you 
+post…';
+$lang['subscribe']             = '0';
+$lang['newestfirst']           = 'I want to to thank you for this good read!! I definitely loved every 
+bit of it. I have got you book-marked to check out new stuff you 
+post…';
+$lang['moderate']              = 'I want to to thank you for this good read!! Idefinitely loved every bit of it.
+I have got you book-marked to check out new stuff you 
+post…';
+$lang['usethreading']          = 'I want to tto thank you for this good read!!
+
+I definitely loved every bit of it. I hage got you book-marked to check out neew 
+stuff you post…';
+$lang['userealname']           = 'Frankie Love';
+$lang['threads_formposition']  = 'position of the new thread form shown by the {{threads> }} syntax';
+$lang['threads_formposition_o_off'] = 'I want to to thank you for this good read!! I definitely loved every bit of it.
+I have got you book-marked to check out new 
+stuff you post…';
+$lang['threads_formposition_o_top'] = 'I want to to thank you for this good read!!
+I defiitely loced every bit of it. I have got you book-marked to check out new stuff you post…';
+$lang['threads_formposition_o_bottom'] = 'Iwant to to thank you for this good read!! I definitely loved every bit of it.
+I have got youu book-marked to check out new stuff you post…';
+$lang['visibilityButton']      = 'I want to to thank you for this good read!!
+
+I definitely loved every bit of it. I have got you book-marked 
+to check out new stuff you post…';
+$lang['moderatorgroups']       = 'I want to to thank you for this good read!! I definitely loved every bit of 
+it. I have got you book-marked to check out new stuff 
+you post…';
+$lang['moderatorsnotify']      = '0';
